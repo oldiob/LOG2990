ba483692af8e3bbc90a20f68521d9d53381aefbf
Dalyna Pak <pocaezz@gmail.com>
2019-09-06 16:00:21 -0400

    DP: add entry point component
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>
    Reviewed-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { DrawAreaComponent } from './draw-area/draw-area.component';
-import { WorkZoneComponent } from './work-zone/work-zone.component';
+import { DrawAreaComponent } from './components/draw-area/draw-area.component';
+import { EntryPointComponent } from './components/entry-point/entry-point.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { WorkZoneComponent } from './components/work-zone/work-zone.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { WorkZoneComponent } from './work-zone/work-zone.component';
     ToolbarComponent,
     DrawAreaComponent,
     WorkZoneComponent,
+    EntryPointComponent,
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/entry-point/entry-point.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'app-entry-point',
+  templateUrl: './entry-point.component.html',
+  styleUrls: ['./entry-point.component.scss'],
+})
+export class EntryPointComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<EntryPointComponent>,
+  ) { }
+
+  ngOnInit() {//
+    }
+
+ close(event: MouseEvent): void {
+    this.dialogRef.close();
+    }
+
+}
