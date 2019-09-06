5b5c1269641d80d3d2acd5baca9831f7e62963cf
Simon Berhanu <simon.berhanu@polymtl.ca>
2019-09-06 17:27:16 -0400

    Create new drawing form
    Signed-off-by: Simon Berhanu <simon.berhanu@polymtl.ca>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,42 +1,27 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+
 import {AppComponent} from './components/app/app.component';
-import { DevPanelComponent } from './components/dev-panel/dev-panel.component';
-import { DrawAreaComponent } from './components/draw-area/draw-area.component';
-import { StrokeComponent } from './components/stroke/stroke.component';
-
-import { WorkZoneComponent } from './components/work-zone/work-zone.component';
-import { KeytrackDirective } from './directive/keytrack.directive';
-import { MousetrackDirective } from './directive/mousetrack.directive';
-
-// Toolbar
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { SelectorComponent } from './components/toolbar/selector/selector.component';
-import { ColorSelectorComponent } from './components/toolbar/selector/color-selector/color-selector.component';
-import { ToolSelectorComponent } from './components/toolbar/selector/tool-selector/tool-selector.component';
-import { ShapeSelectorComponent } from './components/toolbar/selector/shape-selector/shape-selector.component';
-
+import {DrawAreaComponent} from './components/draw-area/draw-area.component';
+import {NewDrawingComponent} from './components/new-drawing/new-drawing.component';
+import {ToolbarComponent} from './components/toolbar/toolbar.component';
+import {WorkZoneComponent} from './components/work-zone/work-zone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarComponent,
     DrawAreaComponent,
     WorkZoneComponent,
-    StrokeComponent,
-    MousetrackDirective,
-    DevPanelComponent,
-    KeytrackDirective,
-
-    ToolbarComponent,
-    SelectorComponent,
-    ColorSelectorComponent,
-    ToolSelectorComponent,
-    ShapeSelectorComponent,
+    NewDrawingComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
