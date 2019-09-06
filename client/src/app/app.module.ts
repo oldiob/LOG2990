b5612a5c33e2dfe5b7009b9facf87203feea7cff
Pan <panbeining@hotmail.com>
2019-09-06 14:58:46 -0400

    Generated basic Component
    Added:
    -draw-area
    -toolbar
    -work-zone

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './components/app/app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { DrawAreaComponent } from './draw-area/draw-area.component';
+import { WorkZoneComponent } from './work-zone/work-zone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarComponent,
+    DrawAreaComponent,
+    WorkZoneComponent,
   ],
   imports: [
     BrowserModule,
