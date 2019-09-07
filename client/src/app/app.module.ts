e17709e7cd678669d90c2f6cd2ebbd7f7281cc5b
Pan <panbeining@hotmail.com>
2019-09-07 10:26:43 -0400

    Add Dev panel and create stroke component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './components/app/app.component';
-import { DevPanelComponent } from './components/dev-panel/dev-panel.component';
-import { DrawAreaComponent } from './components/draw-area/draw-area.component';
-import {NewDrawingComponent} from './components/new-drawing/new-drawing.component';
-import { StrokeComponent } from './components/stroke/stroke.component';
-import { ToolbarOptionComponent } from './components/toolbar/toolbar-option/toolbar-option.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { WorkZoneComponent } from './components/work-zone/work-zone.component';
-import { KeytrackDirective } from './directive/keytrack.directive';
-import { MousetrackDirective } from './directive/mousetrack.directive';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ToolbarComponent,
-    DrawAreaComponent,
-    WorkZoneComponent,
-    NewDrawingComponent,
-    StrokeComponent,
-    MousetrackDirective,
-    DevPanelComponent,
-    ToolbarOptionComponent,
-    KeytrackDirective,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {
-}
-=======
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
@@ -65,4 +24,3 @@ import { ToolbarOptionComponent } from './components/toolbar/toolbar-option/tool
 })
 export class AppModule {
 }
->>>>>>> making toolbar elements one next to another
--- client/src/app/components/dev-panel/dev-panel.component.ts
+++ client/src/app/components/dev-panel/dev-panel.component.ts
@@ -1,25 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dev-panel',
   templateUrl: './dev-panel.component.html',
-  styleUrls: ['./dev-panel.component.scss'],
+  styleUrls: ['./dev-panel.component.scss']
 })
 export class DevPanelComponent implements OnInit {
   @Input() mouseX: number;
   @Input() mouseY: number;
-  @Input() keyEvent: KeyboardEvent;
-  @Input() key: string;
-
-  display = true;
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  disableDisplay() {
-    this.display = false;
   }
 
 }
