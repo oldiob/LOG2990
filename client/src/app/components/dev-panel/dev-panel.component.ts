a409c35c22371e14638719aeae2ae78e94828e2f
Pan <panbeining@hotmail.com>
2019-09-07 10:26:43 -0400

    Add Dev panel and create stroke component

--- client/src/app/components/dev-panel/dev-panel.component.ts
+++ client/src/app/components/dev-panel/dev-panel.component.ts
@@ -1,17 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dev-panel',
   templateUrl: './dev-panel.component.html',
-  styleUrls: ['./dev-panel.component.scss'],
+  styleUrls: ['./dev-panel.component.scss']
 })
 export class DevPanelComponent implements OnInit {
   @Input() mouseX: number;
   @Input() mouseY: number;
-  @Input() keyEvent: KeyboardEvent;
-  @Input() key: string;
-
-  display = true;
 
   constructor() { }
 
