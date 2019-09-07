f2cc488da8b5aedb37a3617dabdb90e93cefe561
Pan <panbeining@hotmail.com>
2019-09-07 11:47:14 -0400

    Add global key listener and WIP on dev panel for more feature

---
+++ client/src/app/directive/keytrack.directive.spec.ts
@@ -0,0 +1,8 @@
+import { KeytrackDirective } from './keytrack.directive';
+
+describe('KeytrackDirective', () => {
+  it('should create an instance', () => {
+    const directive = new KeytrackDirective();
+    expect(directive).toBeTruthy();
+  });
+});
