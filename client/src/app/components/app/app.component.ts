f2d374761ebcdbca16755fd9b524bf13e21645b1
Pan <panbeining@hotmail.com>
2019-09-07 12:32:29 -0400

    dev panel now register key and type properly

--- client/src/app/components/app/app.component.ts
+++ client/src/app/components/app/app.component.ts
@@ -12,6 +12,7 @@ import {IndexService} from '../../services/index/index.service';
 export class AppComponent {
   readonly title: string = 'LOG2990';
   keyEvent: KeyboardEvent;
+  key: string;
   message = new BehaviorSubject<string>('');
 
   constructor(private basicService: IndexService) {
@@ -23,6 +24,11 @@ export class AppComponent {
   }
   @HostListener('document:keypress', ['$event']) // need refactor
   handleKeyboardEvent(event: KeyboardEvent) {
+    this.keyEvent = event;
+    this.key = this.keyEvent.key;
+  }
+  @HostListener('document:keydown', ['$event']) // need refactor
+  handleKeyboardEventDown(event: KeyboardEvent) {
     this.keyEvent = event;
   }
 }
