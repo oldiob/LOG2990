b585c90d9ff6d752eac841ea68836740d3be61c9
ross <rmyhovych@gmail.com>
2019-09-08 19:11:23 -0400

    adding components for different toolbar options

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,23 +5,34 @@ import {AppComponent} from './components/app/app.component';
 import { DevPanelComponent } from './components/dev-panel/dev-panel.component';
 import { DrawAreaComponent } from './components/draw-area/draw-area.component';
 import { StrokeComponent } from './components/stroke/stroke.component';
-import { ToolbarOptionComponent } from './components/toolbar/toolbar-option/toolbar-option.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
 import { WorkZoneComponent } from './components/work-zone/work-zone.component';
 import { KeytrackDirective } from './directive/keytrack.directive';
 import { MousetrackDirective } from './directive/mousetrack.directive';
 
+// Toolbar
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SelectorComponent } from './components/toolbar/selector/selector.component';
+import { ColorSelectorComponent } from './components/toolbar/selector/color-selector/color-selector.component';
+import { ToolSelectorComponent } from './components/toolbar/selector/tool-selector/tool-selector.component';
+import { ShapeSelectorComponent } from './components/toolbar/selector/shape-selector/shape-selector.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
     DrawAreaComponent,
     WorkZoneComponent,
     StrokeComponent,
     MousetrackDirective,
     DevPanelComponent,
-    ToolbarOptionComponent,
     KeytrackDirective,
+
+    ToolbarComponent,
+    SelectorComponent,
+    ColorSelectorComponent,
+    ToolSelectorComponent,
+    ShapeSelectorComponent,
   ],
   imports: [
     BrowserModule,
