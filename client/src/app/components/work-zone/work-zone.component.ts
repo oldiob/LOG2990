953f25b522efbf3814b35aa782cf7a528185dcd0
Simon Berhanu <simon1515@hotmail.com>
2019-09-09 00:32:22 -0400

    Sync up form and work-area component
    Signed-off-by: Simon Berhanu <simon1515@hotmail.com>

--- client/src/app/components/work-zone/work-zone.component.ts
+++ client/src/app/components/work-zone/work-zone.component.ts
@@ -1,23 +1,60 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { WorkZoneService } from './../../services/work-zone.service';
 
 @Component({
   selector: 'app-work-zone',
   templateUrl: './work-zone.component.html',
-  styleUrls: ['./work-zone.component.scss']
+  styleUrls: ['./work-zone.component.scss'],
 })
-export class WorkZoneComponent implements OnInit {
+export class WorkZoneComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('workZone', { static: true }) workZone: ElementRef;
+
   @Input() keyEvent: KeyboardEvent;
   @Input() key: string;
+
+  width: number;
   height: number;
-  widget: number;
   backgroundColor: number;
   height: number;
   widget: number;
   backgroundColor: number;
 
-  constructor() { }
+  maxWidth: number;
+  maxHeight: number;
+
+  constructor(private workZoneService: WorkZoneService) { }
+
+  updateSize(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  // Assigning size values from 'work-zone' referenced element
+  updateMaxSize() {
+    this.maxWidth = this.workZone.nativeElement.offsetWidth;
+    this.maxHeight = this.workZone.nativeElement.offsetHeight;
+    this.workZoneService.updateInitialDimensions(this.maxWidth, this.maxHeight);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    // Update  work-area size values when resizing window
+    this.updateMaxSize();
+  }
 
   ngOnInit() {
+    this.updateMaxSize();
+
+    // Subscribes to WorkZoneService observables
+    this.workZoneService.currentHeight
+      .subscribe((width) => this.width = width);
+    this.workZoneService.currentHeight
+      .subscribe((height) => this.height = height);
+    this.workZoneService.currentHeight
+      .subscribe((backgroundColor) => this.backgroundColor = backgroundColor);
   }
+
+  ngAfterViewInit(): void { }
 
 }
