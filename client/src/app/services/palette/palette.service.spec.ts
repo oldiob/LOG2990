843a9c89a2af1be2cf3099f5d2f5e90e2d6b6aa0
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-09 18:18:31 -0400

    Fix lint styles
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>
    Reviewed-by: Bei Ning Pan <bei-nin.pan@polymtl.ca>

--- client/src/app/services/palette/palette.service.spec.ts
+++ client/src/app/services/palette/palette.service.spec.ts
@@ -1,37 +1,37 @@
 import { ColorPalette } from './palette.service';
 
 describe('Service-ColorPalette', () => {
-    const primary : number = 0;
-    const secondary : number = 0xFFFFFFFF;
-    var mock : ColorPalette = new ColorPalette(primary, secondary);
+    const PRIMARY = 0;
+    const SECONDARY = 0xFFFFFFFF;
+    let mock: ColorPalette = new ColorPalette(PRIMARY, SECONDARY);
 
     beforeEach(() => {
-	mock = new ColorPalette(primary, secondary);
+        mock = new ColorPalette(PRIMARY, SECONDARY);
     });
 
     it('should construct correctly', () => {
-	expect(mock.primary).toEqual(primary);
-	expect(mock.secondary).toEqual(secondary);
+        expect(mock.primary).toEqual(PRIMARY);
+        expect(mock.secondary).toEqual(SECONDARY);
     });
 
     it('should swap correctly', () => {
-	mock.swap();
-	expect(mock.primary).toEqual(secondary);
-	expect(mock.secondary).toEqual(primary);
+        mock.swap();
+        expect(mock.primary).toEqual(SECONDARY);
+        expect(mock.secondary).toEqual(PRIMARY);
     });
 
     it('should select the correct color', () => {
-	const new_primary : number = 0xdeadbeef;
-	const new_secondary : number = 0xafafaf;
+        const NEW_PRIMARY = 0xdeadbeef;
+        const NEW_SECONDARY = 0xafafaf;
 
-	// Expect no side effect on secondary
-	mock.selectPrimary(new_primary);
-	expect(mock.primary).toEqual(new_primary);
-	expect(mock.secondary).toEqual(secondary);
+        // Expect no side effect on secondary
+        mock.selectPrimary(NEW_PRIMARY);
+        expect(mock.primary).toEqual(NEW_PRIMARY);
+        expect(mock.secondary).toEqual(SECONDARY);
 
-	// Expect no side effect on primary
-	mock.selectSecondary(new_secondary);
-	expect(mock.primary).toEqual(new_primary);
-	expect(mock.secondary).toEqual(new_secondary);
+        // Expect no side effect on primary
+        mock.selectSecondary(NEW_SECONDARY);
+        expect(mock.primary).toEqual(NEW_PRIMARY);
+        expect(mock.secondary).toEqual(NEW_SECONDARY);
     });
 });
--- client/src/app/utils/ring_buffer.ts
+++ client/src/app/utils/ring_buffer.ts
@@ -1,15 +1,16 @@
 export class RingBuffer<T> {
-    readonly len   : number;
-    index : number;
-    arr   : Array<T>;
-    constructor(len : number) {
-	this.index = 0;
-	this.len = len;
-	this.arr = new Array<T>(len);
+    readonly len: number;
+    index: number;
+    arr: T[];
+    constructor(len: number) {
+        this.index = 0;
+        this.len = len;
+        this.arr = new Array<T>(len);
     }
-    add(x : T) {
-	let i = this.index % this.len;
-	this.index += 1;
-	this.arr[i] = x;
+    add(x: T) {
+        this.arr[this.index++] = x;
+        if (this.index >= this.len) {
+            this.index = 0;
+        }
     }
 }
