4946b75572f7d124e3b1b9f1c66f8182fa3da000
Tristan Philis <46928560+TristanPhilis@users.noreply.github.com>
2019-09-13 17:01:08 -0400

    Write the start code of the Rectangle Feature
    Signed-off-by: Tristan Philis <tristan.philis@gmail.com>

--- client/src/app/services/rectangle/rectangle-service.ts
+++ client/src/app/services/rectangle/rectangle-service.ts
@@ -1,86 +1,40 @@
-import { HostListener } from '@angular/core';
-import { Rectangle } from '../../../../../common/communication/rectangle';
+// import { HostListener } from '@angular/core';
 
+// export class RectangleService {
+//     selectRect: EventTarget;
+//     currentX: number;
+//     currentY: number;
 
-export class RectangleService {
-    currentX = 0;
-    currentY = 0;
-    mouseX = 0;
-    mouseY = 0;
-    width = 0;
-    height = 0;
-    i = 0;
-    click: boolean;
-    rectangles: Rectangle[];
-    rectangle: Rectangle;
-    constructor() {
-        this.click = false;
-        this.rectangles = [];
-    }
+//     constructor() {
+//         this.selectRect = new EventTarget();
+//     }
 
-    @HostListener('window: mousedown', ['$event'])
-    startDrawRect(event: MouseEvent): void {
-        this.mouseX = event.offsetX;
-        this.mouseY = event.offsetY;
-        this.currentX = this.mouseX;
-        this.currentY = this.mouseY;
-        this.click = true;
-    }
-    @HostListener('mousemove', ['$event'])
-    drawRect(event: MouseEvent): void {
-        if (this.click) {
-        this.width = this.mouseX - event.offsetX;
-        this.height = this.mouseY - event.offsetY;
-        
-      
+//     @HostListener('window: mousedown', ['$event'])
+//     startDragMouse(event: MouseEvent): void {
+//         if(event.target.)
+//         this.currentX = event.clientX;
+//         this.currentY = event.clientY;
+//         this.selectRect = event.target;
+//         event.preventDefault();
+//     }
 
-            if (this.width>=0) { 
-                this.currentX= event.offsetX;
-                this.width = (Math.abs(this.mouseX - event.offsetX));
-            } 
-            else if (this.width<0) {
-                this.currentX=this.mouseX;
-                this.width = (Math.abs(this.mouseX - event.offsetX));
-            }     
-  
-            if(this.height>=0){
-                this.currentY=event.offsetY;
-                this.height = (Math.abs(this.mouseY - event.offsetY));
-            }
-            else if(this.height<0){
-                this.currentY=this.mouseY;
-                this.height = (Math.abs(this.mouseY - event.offsetY));
-            }       
-             
-        }
-}
+//     @HostListener('window: mousemove', ['$event'])
+//     dragMouse(event: MouseEvent): void {
+//         if (this.selectRect) {
+//             const x: number = this.selectRect.getAttribute('x') + event.clientX - this.currentX;
+//             const y: number = this.selectRect.getAttribute('y') + event.clientY - this.currentY;
+//             this.currentX = event.clientX;
+//             this.currentY = event.clientY;
+//         }
+//     }
+//     @HostListener('window: mouseleave', ['$event'])
+//     endDragMouse(event: MouseEvent): void {
+//         this.selectRect = null;
+//     }
 
-    @HostListener('window: mouseleave', ['$event'])
-    @HostListener('window: mouseup', ['$event'])
-    endDragMouse(event: MouseEvent): void {
-            this.addRectangle();
-            this.click = false;
-    }
+//     squareCtrl(event: KeyboardEvent): void {
+//         if (event.ctrlKey) {
 
-    @HostListener('window: keydown', ['$event'])
-    squareShift(event: KeyboardEvent): void {
-        if (event.shiftKey) {
-            this.width = this.height;
-            this.height = this.width;
-        }
-    }
-
-    addRectangle(): void {
-        this.rectangle = {
-            pointX: this.currentX,
-            pointY: this.currentY,
-            width: this.width,
-            height: this.height,
-        };
-        this.rectangles.push(this.rectangle);
-        // tslint:disable-next-line:forin
-        for (const rect in this.rectangles) {
-            console.log('RECT', this.rectangles[rect]);
-            }
-        }
-}
+//         }
+//     }
+// }
