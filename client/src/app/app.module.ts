ad2d7a48dfb14df39d817481d0146c88362c4645
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-13 16:49:59 -0400

    Fix style for second merge - v1.0.0
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS, MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDialogRef,
+    MatDividerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTableModule
+} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './components/app/app.component';
 import { DevPanelComponent } from './components/dev-panel/dev-panel.component';
 import { DrawAreaComponent } from './components/draw-area/draw-area.component';
+import { EntryPointComponent } from './components/entry-point/entry-point.component';
 import { NewDrawingComponent } from './components/new-drawing/new-drawing.component';
 import { PolyDessinComponent } from './components/poly-dessin/poly-dessin.component';
 import { ColorSelectorComponent } from './components/toolbar/selector/color-selector/color-selector.component';
@@ -16,11 +21,6 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { WorkZoneComponent } from './components/work-zone/work-zone.component';
 import { KeytrackDirective } from './directive/keytrack.directive';
 import { MousetrackDirective } from './directive/mousetrack.directive';
-import {
-    MAT_DIALOG_DATA, MatButtonModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDialogRef, MatDividerModule,
-    MatFormFieldModule, MatInputModule, MatSelectModule, MatTableModule, MAT_DIALOG_DEFAULT_OPTIONS
-} from '@angular/material';
-import { EntryPointComponent } from './components/entry-point/entry-point.component';
 
 @NgModule({
     declarations: [
