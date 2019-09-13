096b9119c1f636c5bcb2c93fcdc724e1c3237728
ross <rmyhovych@gmail.com>
2019-09-13 14:58:53 -0400

    removing stroke from module.ts
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {DevPanelComponent} from './components/dev-panel/dev-panel.component';
 import {DrawAreaComponent} from './components/draw-area/draw-area.component';
 import {NewDrawingComponent} from './components/new-drawing/new-drawing.component';
 import {PolyDessinComponent } from './components/poly-dessin/poly-dessin.component';
-import {StrokeComponent} from './components/stroke/stroke.component';
 import {ColorSelectorComponent} from './components/toolbar/selector/color-selector/color-selector.component';
 import {SelectorComponent} from './components/toolbar/selector/selector.component';
 import {ShapeSelectorComponent} from './components/toolbar/selector/shape-selector/shape-selector.component';
@@ -22,7 +21,6 @@ import {MousetrackDirective} from './directive/mousetrack.directive';
     AppComponent,
     DrawAreaComponent,
     WorkZoneComponent,
-    StrokeComponent,
     MousetrackDirective,
     DevPanelComponent,
     NewDrawingComponent,
