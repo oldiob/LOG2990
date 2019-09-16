f60a76b39de61113bf81f66b8e16bb219f272210
Pan <panbeining@hotmail.com>
2019-09-15 22:19:23 -0400

    add svg basic and test

--- client/src/app/draw-area/draw-area.component.ts
+++ client/src/app/draw-area/draw-area.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { WorkZoneService } from 'src/services/work-zone.service';
+import { Component, ComponentFactory, ComponentFactoryResolver, ComponentRef,
+   Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { WorkZoneService } from './../../services/work-zone.service';
+import { GenericStrokeComponent } from '../generic-stroke/generic-stroke.component';
 
 @Component({
     selector: 'app-draw-area',
@@ -7,17 +9,23 @@ import { WorkZoneService } from 'src/services/work-zone.service';
     styleUrls: ['./draw-area.component.scss'],
 })
 export class DrawAreaComponent implements OnInit {
+    @ViewChild('svgContainer', { static: true, read: ViewContainerRef }) entry: ViewContainerRef;
     @Input() keyEvent: KeyboardEvent;
     @Input() key: string;
-    height: number = 0;
-    width: number = 0;
+
+    mouseX: number;
+    mouseY: number;
+
+    componentRef: ComponentRef<any>;
+    height: number;
+    width: number;
     rectangleWidth: number;
     rectangleHeight: number;
     rectangleActivate = false;
     backgroundColor: string = "#ffffffff";
     currentStyles: { height: number; width: number; 'background-color': string; };
-    constructor(private workZoneService: WorkZoneService) {
-    }
+
+    constructor(private workZoneService: WorkZoneService, private resolver: ComponentFactoryResolver) { }
 
     ngOnInit() {
         // Subscribes to WorkZoneService observables
@@ -37,5 +45,33 @@ export class DrawAreaComponent implements OnInit {
             width: `${this.width}px`,
             'background-color': `${this.backgroundColor}`,
         };
+    }
+    coordinates(event: MouseEvent): void {
+        this.mouseX = event.offsetX;
+        this.mouseY = event.offsetY;
+    }
+
+    onClick(event: MouseEvent): void {
+        this.mouseX = event.clientX;
+        this.mouseY = event.clientY;
+        this.createComponent('danger');
+    }
+
+    createComponent(type: any) {
+      const factory: ComponentFactory<any> = this.resolver.resolveComponentFactory(GenericStrokeComponent);
+
+      this.componentRef = this.entry.createComponent(factory);
+
+      this.componentRef.instance.type = type;
+
+      this.componentRef.instance.backgroundColor = 'black';
+      this.componentRef.instance.style.left = this.mouseX + 'px';
+
+
+      //this.componentRef.instance.output.subscribe((event: any) => console.log(event));
+
+    }
+    ngOnDestroy() {
+      this.componentRef.destroy();
     }
 }
---
+++ client/src/app/generic-stroke/generic-stroke.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-stroke',
+  templateUrl: './generic-stroke.component.html',
+  styleUrls: ['./generic-stroke.component.scss']
+})
+export class GenericStrokeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
