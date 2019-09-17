c25c8988ba108b8fef351a0c19c933cae151e04e
Dalyna Pak <pocaezz@gmail.com>
2019-09-17 00:09:51 -0400

    Add Svg Rectangle Width And Height
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- client/src/app/draw-area/draw-area.component.ts
+++ client/src/app/draw-area/draw-area.component.ts
@@ -66,7 +66,10 @@ export class DrawAreaComponent implements OnInit {
     // @HostListener('window: mousedown', ['$event'])
 
     onClick(event: MouseEvent): void {
-        this.mouseX = event.offsetX;
-        this.mouseY = event.offsetY;
+        this.mouseX = event.clientX;
+        this.mouseY = event.clientY;
+        this.rectangleHeight = this.mouseY;
+        this.rectangleWidth = this.mouseX;
+        this.rectangleActivate = true;
     }
 }
