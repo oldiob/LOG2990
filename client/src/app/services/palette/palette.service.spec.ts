78c531ec5843edd2c71dd6cd87688e56de9694ef
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-16 21:46:18 -0400

    Add tests for palette service
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/services/palette/palette.service.spec.ts
+++ client/src/app/services/palette/palette.service.spec.ts
@@ -1,37 +1,52 @@
+import { TestBed, inject } from '@angular/core/testing';
+
 import { ColorPalette } from './palette.service';
 
 describe('Service-ColorPalette', () => {
-    const PRIMARY = 0;
-    const SECONDARY = 0xFFFFFFFF;
-    let mock: ColorPalette = new ColorPalette(PRIMARY, SECONDARY);
-
     beforeEach(() => {
-        mock = new ColorPalette(PRIMARY, SECONDARY);
+        TestBed.configureTestingModule({
+            providers: [ColorPalette]
+        });
     });
 
-    it('should construct correctly', () => {
-        expect(mock.primary).toEqual(PRIMARY);
-        expect(mock.secondary).toEqual(SECONDARY);
-    });
+    it('should construct correctly', inject([ColorPalette], (service: ColorPalette) => {
+        expect(service).toBeTruthy();
+        expect(service.primary).
+            toEqual(ColorPalette.DEFAULT_PRIMARY);
+        expect(service.secondary).
+            toEqual(ColorPalette.DEFAULT_SECONDARY);
+        expect(service.previous.arr).
+            toEqual(new Array<number>(ColorPalette.MAX_HISTORY).fill(ColorPalette.DEFAULT_MEMSET));
+    }));
 
-    it('should swap correctly', () => {
-        mock.swap();
-        expect(mock.primary).toEqual(SECONDARY);
-        expect(mock.secondary).toEqual(PRIMARY);
-    });
+    it('should swap correctly', inject([ColorPalette], (service: ColorPalette) => {
+        service.swap();
+        expect(service.primary).toEqual(ColorPalette.DEFAULT_SECONDARY);
+        expect(service.secondary).toEqual(ColorPalette.DEFAULT_PRIMARY);
+    }));
 
-    it('should select the correct color', () => {
+    it('should select the correct color', inject([ColorPalette], (service: ColorPalette) => {
         const NEW_PRIMARY = 0xdeadbeef;
         const NEW_SECONDARY = 0xafafaf;
 
         // Expect no side effect on secondary
-        mock.selectPrimary(NEW_PRIMARY);
-        expect(mock.primary).toEqual(NEW_PRIMARY);
-        expect(mock.secondary).toEqual(SECONDARY);
+        service.selectPrimary(NEW_PRIMARY);
+        expect(service.primary).toEqual(NEW_PRIMARY);
+        expect(service.secondary).toEqual(ColorPalette.DEFAULT_SECONDARY);
 
         // Expect no side effect on primary
-        mock.selectSecondary(NEW_SECONDARY);
-        expect(mock.primary).toEqual(NEW_PRIMARY);
-        expect(mock.secondary).toEqual(NEW_SECONDARY);
-    });
+        service.selectSecondary(NEW_SECONDARY);
+        expect(service.primary).toEqual(NEW_PRIMARY);
+        expect(service.secondary).toEqual(NEW_SECONDARY);
+    }));
+
+    it('should format the colors correctly', inject([ColorPalette], (service: ColorPalette) => {
+        expect(service.getPrimary())
+            .toEqual(`#${ColorPalette.DEFAULT_PRIMARY.toString(16)}`)
+        expect(service.getSecondary())
+            .toEqual(`#${ColorPalette.DEFAULT_SECONDARY.toString(16)}`);
+        const history: string[] = service.getHistory();
+        history.forEach((value: string) => expect(value)
+            .toEqual(`#${ColorPalette.DEFAULT_MEMSET.toString(16)}`))
+    }));
 });
