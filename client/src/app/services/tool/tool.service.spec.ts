4098eb9e749e4840b0ab7c2a34a70f43e3ed3182
ross <rmyhovych@gmail.com>
2019-09-20 14:37:43 -0400

    Covering ToolCategory
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/app/services/tool/tool.service.spec.ts
+++ client/src/app/services/tool/tool.service.spec.ts
@@ -1,7 +1,7 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { ToolService } from './tool.service';
 
-describe('Service: CurrentTool', () => {
+fdescribe('Service: CurrentTool', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			providers: [ToolService],
