ec5ca2ebea04a2394e34d4009bb3ab9c05687c7c
ross <rmyhovych@gmail.com>
2019-09-19 23:28:14 -0400

    Using the color palette service in ColorOptionComponent
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/app/components/toolbar/color-option/color-option.component.ts
+++ client/src/app/components/toolbar/color-option/color-option.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PaletteService } from 'src/app/services/palette/palette.service';
 
 @Component({
   selector: 'app-color-option',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorOptionComponent implements OnInit {
 
-  constructor() { }
+  private paletteService: PaletteService;
+
+  constructor(paletteService: PaletteService) { 
+    this.paletteService = paletteService;
+
+    this.paletteService.selectPrimary(3213);
+    this.paletteService.selectSecondary(18621);
+  }
 
   ngOnInit() {
+  }
+
+  getPrimary(): string {
+    return this.paletteService.getPrimary();
+  }
+
+  getSecondary(): string {
+    return this.paletteService.getSecondary();
+  }
+
+  swap(): void {
+    this.paletteService.swap();
   }
 
 }
