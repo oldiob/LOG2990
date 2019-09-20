2c92bba2691d3799a71e3def9e2ef3e1c4267fb6
Simon Berhanu <simon1515@hotmail.com>
2019-09-20 01:18:58 -0400

    Finish NewDrawingComponent's tests

--- client/src/app/components/new-drawing/new-drawing.component.spec.ts
+++ client/src/app/components/new-drawing/new-drawing.component.spec.ts
@@ -1,18 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { WorkZoneService } from 'src/app/services/work-zone.service';
 import { NewDrawingComponent } from './new-drawing.component';
 
 fdescribe('NewDrawingComponent', () => {
   let newDrawingComponent: NewDrawingComponent;
-  let fixture: ComponentFixture<NewDrawingComponent>;
-  // let workZoneService: WorkZoneService;
-  // let formBuidler: FormBuilder;
+  let workZoneService: WorkZoneService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule],
       declarations: [NewDrawingComponent],
       providers: [
         { provide: WorkZoneService },
@@ -26,37 +25,75 @@ fdescribe('NewDrawingComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NewDrawingComponent);
-    newDrawingComponent = fixture.componentInstance;
-    fixture.detectChanges();
-
-    // workZoneService = TestBed.get(WorkZoneService);
-    // formBuidler = TestBed.get(FormBuilder);
+    // Injection of FromBuilder and workZoneService
+    workZoneService = new WorkZoneService();
+    newDrawingComponent = new NewDrawingComponent(new FormBuilder(), workZoneService);
+    newDrawingComponent.ngOnInit();
   });
 
-  it('should create', () => {
+  it('should create a new drawing component', () => {
     expect(newDrawingComponent).toBeTruthy();
   });
 
-  describe('createForm() method', () => {
-    it('should create a form', () => {
-      expect(newDrawingComponent.newDrawingFrom).toBeTruthy();
-    });
-    it('should create a height form control', () => {
-      expect(newDrawingComponent.newDrawingFrom.contains('height')).toBeTruthy();
-    });
-    it('should create a width form control', () => {
-      expect(newDrawingComponent.newDrawingFrom.contains('width')).toBeTruthy();
-    });
-    it('should create a background form control', () => {
-      expect(newDrawingComponent.newDrawingFrom.contains('backgroundColor')).toBeTruthy();
-    });
+  it('should create a form', () => {
+    expect(newDrawingComponent.newDrawingFrom).toBeTruthy();
   });
 
-  // describe('onSubmit() method', () => {
-  //   it('should create a form', () => {
-  //     expect(newDrawingComponent.newDrawingFrom).toBeTruthy();
-  //   });
-  // });
+  it('#createForm should create a width, height and background color form input', () => {
+    expect(newDrawingComponent.newDrawingFrom.contains('width')).toBeTruthy();
+    expect(newDrawingComponent.newDrawingFrom.contains('height')).toBeTruthy();
+    expect(newDrawingComponent.newDrawingFrom.contains('backgroundColor')).toBeTruthy();
+  });
+
+  it('should get width, height and background form input values', () => {
+    expect(newDrawingComponent.width).toBe(newDrawingComponent.newDrawingFrom.controls.width.value);
+    expect(newDrawingComponent.height).toBe(newDrawingComponent.newDrawingFrom.controls.height.value);
+    expect(newDrawingComponent.backgroundColor).toBe(newDrawingComponent.newDrawingFrom.controls.backgroundColor.value);
+  });
+
+  it('#onSubmit should update draw area height, width and background color', () => {
+    const WIDTH = newDrawingComponent.width;
+    const HEIGHT = newDrawingComponent.height;
+    const BACKGROUND_COLOR = newDrawingComponent.backgroundColor;
+
+    newDrawingComponent.onSubmit();
+
+    workZoneService.currentWidth.subscribe(
+      (currentHeight) => expect(currentHeight).toBe(WIDTH),
+    );
+    workZoneService.currentHeight.subscribe(
+      (currentHeight) => expect(currentHeight).toBe(HEIGHT),
+    );
+    workZoneService.currentBackgroundColor.subscribe(
+      (currentBackgroundColor) => expect(currentBackgroundColor).toBe(BACKGROUND_COLOR),
+    );
+  });
+
+  it('#onSubmit should raise displayChange event', () => {
+    let isShowNewDrawing = true;
+    newDrawingComponent.displayChange.subscribe(
+      (isShow: boolean) => isShowNewDrawing = isShow,
+    );
+    newDrawingComponent.onSubmit();
+    expect(isShowNewDrawing).toBeFalsy();
+  });
+
+  it('should change background color form control', () => {
+    const BACKGROUND_COLOR = '#CCCCCC';
+    newDrawingComponent.chooseBgColor(BACKGROUND_COLOR);
+    expect(newDrawingComponent.newDrawingFrom.controls.backgroundColor.value).toBe(BACKGROUND_COLOR);
+
+  });
+
+  it('should fetch default dimensions from work zone service and update its default dimensions', () => {
+    workZoneService.currentMaxWidth.subscribe((maxWidth) => {
+      newDrawingComponent.newDrawingFrom.controls.width.setValue(maxWidth);
+      expect(newDrawingComponent.width).toBe(maxWidth);
+    });
+    workZoneService.currentMaxWidth.subscribe((maxHeight) => {
+      newDrawingComponent.newDrawingFrom.controls.height.setValue(maxHeight);
+      expect(newDrawingComponent.height).toBe(maxHeight);
+    });
+  });
 
 });
