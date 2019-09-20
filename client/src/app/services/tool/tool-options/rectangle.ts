e44e1f1d158c88897d4d082d551f14ae9105a38e
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-20 17:22:12 -0400

    Example of how a tools should use its interface
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/services/tool/tool-options/rectangle.ts
+++ client/src/app/services/tool/tool-options/rectangle.ts
@@ -1,14 +1,26 @@
 import { ITool } from './i-tool';
+import { SVGRect } from 'src/app/services/svg/svg.ect';
 
 export class Rectangle implements ITool {
 
+    private currentRect: SVGRect | null = null;
+
     constructor() { }
 
-    leftClick() {
-        throw new Error('Method not implemented.');
+    onPressed(event: MouseEvent): SVGInterface {
+        let x: number = event.clientX;
+        let y: number = event.clientY;
+        this.currentRect = new SVGRect(x, y);
+        return this.currentRect;
     }
-    leftRelease() {
-        throw new Error('Method not implemented.');
+    onRelease(event: MouseEvent) {
+        this.currentRect = null;
     }
-
+    onMotion(event: MouseEvent) {
+        if (this.currentRect == null)
+            return;
+        let x: number = event.clientX;
+        let y: number = event.clientY;
+        this.currentRect.setP2(x, y);
+    }
 }
