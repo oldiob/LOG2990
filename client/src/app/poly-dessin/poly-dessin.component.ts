628f1a6e0793c5afdddcea76df6a841095270e75
Simon Berhanu <simon1515@hotmail.com>
2019-09-22 18:01:47 -0400

    Add Angular Material to NewDrawingComponent
    New drawing form is now opened from polyDessin
    Add a background div to its modal to prevent outside clicks from
    registering

--- client/src/app/poly-dessin/poly-dessin.component.ts
+++ client/src/app/poly-dessin/poly-dessin.component.ts
@@ -1,48 +1,59 @@
 import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { IndexService } from 'src/services/index/index.service';
 import { Message } from '../../../../common/communication/message';
+import { IndexService } from '../../services/index/index.service';
 import { NewDrawingComponent } from '../new-drawing/new-drawing.component';
 
 @Component({
-    selector: 'app-poly-dessin',
-    templateUrl: './poly-dessin.component.html',
-    styleUrls: ['./poly-dessin.component.scss'],
+  selector: 'app-poly-dessin',
+  templateUrl: './poly-dessin.component.html',
+  styleUrls: ['./poly-dessin.component.scss'],
 })
 export class PolyDessinComponent implements OnInit {
 
-    constructor(private basicService: IndexService) {
-        this.basicService.basicGet()
-            .pipe(
-                map((message: Message) => `${message.title} ${message.body}`),
-            )
-            .subscribe(this.message);
-    }
-    readonly title: string = 'LOG2990';
-    keyEvent: KeyboardEvent;
-    key: string;
+  constructor(private basicService: IndexService,
+              private dialog: MatDialog,
+  ) {
+    this.basicService.basicGet()
+      .pipe(
+        map((message: Message) => `${message.title} ${message.body}`),
+      )
+      .subscribe(this.message);
+  }
+  readonly title: string = 'LOG2990';
+  keyEvent: KeyboardEvent;
+  key: string;
 
-    message = new BehaviorSubject<string>('');
-    displayNewDrawing = true;
-    @ViewChild(NewDrawingComponent, { static: false })
-    newDrawingComponent: NewDrawingComponent;
+  message = new BehaviorSubject<string>('');
+  displayNewDrawing = true;
+  @ViewChild(NewDrawingComponent, { static: false })
+  newDrawingComponent: NewDrawingComponent;
 
-    ngOnInit() {
-        // this.displayNewDrawing = this.newDrawingComponent.displayNewDrawing;
-    }
-    @HostListener('document:keypress', ['$event']) // need refactor
-    onKeyPressed(event: KeyboardEvent) {
-        this.keyEvent = event;
-        this.key = this.keyEvent.key;
-    }
-    @HostListener('document:keydown', ['$event']) // need refactor
-    onKeyDown(event: KeyboardEvent) {
-        this.keyEvent = event;
-        this.key = '';
-    }
-    changeDisplay(display: boolean) {
-        this.displayNewDrawing = display;
-    }
+  ngOnInit() {
+    this.openModal();
+  }
+
+  openModal() {
+    this.dialog.open(NewDrawingComponent, {
+      height: '670px',
+      width: '400px',
+    });
+  }
+
+  @HostListener('document:keypress', ['$event']) // need refactor
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.keyEvent = event;
+    this.key = this.keyEvent.key;
+  }
+  @HostListener('document:keydown', ['$event']) // need refactor
+  handleKeyboardEventDown(event: KeyboardEvent) {
+    this.keyEvent = event;
+    this.key = '';
+  }
+  changeDisplay(display: boolean) {
+    this.displayNewDrawing = display;
+  }
 
 }
