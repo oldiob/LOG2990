f7a710037f81d5547748c03a8ac69ba90a51a4e7
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-22 23:25:55 -0400

    Add coverage for color-option component
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/toolbar/color-option/color-option.component.spec.ts
+++ client/src/app/toolbar/color-option/color-option.component.spec.ts
@@ -22,4 +22,12 @@ describe('ColorOptionComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should swap color correctly', () => {
+        const primary: string = component.primary;
+        const secondary: string = component.secondary;
+        component.swap();
+        expect(component.primary).toEqual(secondary);
+        expect(component.secondary).toEqual(primary);
+    });
 });
