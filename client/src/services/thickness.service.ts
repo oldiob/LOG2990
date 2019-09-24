4600956a8ceec1f55fe0b079a753e49d278eb09a
ross <rmyhovych@gmail.com>
2019-09-23 22:07:57 -0400

    Adding thickness getter and setter
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/thickness.service.ts
+++ client/src/services/thickness.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ThicknessService {
+  private thickness: number;
 
-  constructor() { }
+  constructor() { 
+    this.setThickness(1);
+  }
+
+  setThickness(thickness: number): void {
+    if (thickness <= 0 || thickness > 20) {
+      throw new Error("Invalid thickness " + thickness + ". Must be ]0, 20].")
+    }
+
+    this.thickness = thickness;
+  }
+
+  getThickness(): number {
+    return this.thickness;
+  }
 }
