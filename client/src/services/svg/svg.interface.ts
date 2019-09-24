1571364f2e3cc20e90d17f9613f501832b663d27
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-23 22:10:16 -0400

    Change interface of SVGInterface
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/services/svg/svg.interface.ts
+++ client/src/services/svg/svg.interface.ts
@@ -1,8 +1,8 @@
 export interface SVGInterface {
     isAt(x: number, y: number): boolean;
     isIn(x: number, y: number, r: number): boolean;
-    setPrimary(color: number): void;
-    setSecondary(color: number): void;
-    getPrimary(): number;
-    getSecondary(): number;
+    setPrimary(color: string): void;
+    setSecondary(color: string): void;
+    getPrimary(): string;
+    getSecondary(): string;
 }
