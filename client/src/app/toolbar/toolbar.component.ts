3ec57481051d2ca315d1801c26a4e35c12b36d1c
Olivier Dion <olivier.dion@polymtl.ca>
2019-09-24 21:21:33 -0400

    Fix style and remove color option html from tool bar
    This is the responsability of the color-option component, not the
    toolbar itself.
    
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/app/toolbar/toolbar.component.ts
+++ client/src/app/toolbar/toolbar.component.ts
@@ -1,78 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { PaletteService } from 'src/services/palette/palette.service';
 import { ToolService } from 'src/services/tool/tool.service';
 
 export enum OptionType {
-	COLOR = 0,
-	TOOL = 1,
+    TOOL = 0
 };
 
 
 @Component({
-	selector: 'app-toolbar',
-	templateUrl: './toolbar.component.html',
-	styleUrls: ['./toolbar.component.scss'],
+    selector: 'app-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-	private FILE_LOCATION: string = "../../../assets/images/";
+    private FILE_LOCATION: string = "../../../assets/images/";
 
-	currentDisplayedOption: OptionType;
-	optionDisplayed: boolean;
+    currentDisplayedOption: OptionType;
+    optionDisplayed: boolean;
 
-	constructor(private toolService: ToolService, private paletteService: PaletteService) {
-		this.currentDisplayedOption = OptionType.TOOL;
-		this.optionDisplayed = false;
-	}
+    constructor(private toolService: ToolService) {
+        this.currentDisplayedOption = OptionType.TOOL;
+        this.optionDisplayed = false;
+    }
 
-	ngOnInit() {
-		//
-	}
+    ngOnInit() {
+        //
+    }
 
-	getPrimaryColor(): string {
-		return this.paletteService.getPrimary();
-	}
-
-	getSecondaryColor(): string {
-		return this.paletteService.getSecondary();
-	}
-
-	private displayOption(optionType: OptionType): void {
-		this.optionDisplayed = this.optionDisplayed == true ? this.currentDisplayedOption != optionType : true;
-		this.currentDisplayedOption = optionType;
-	} 
+    private displayOption(optionType: OptionType): void {
+        this.optionDisplayed = this.optionDisplayed == true ? this.currentDisplayedOption != optionType : true;
+        this.currentDisplayedOption = optionType;
+    }
 
 
-	getButtonFilesource(category: number): string {
-		return this.FILE_LOCATION + this.toolService.getToolCategoryFilename(category);
-	}
+    getButtonFilesource(category: number): string {
+        return this.FILE_LOCATION + this.toolService.getToolCategoryFilename(category);
+    }
 
 
-	getOptionTopMargin(): number {
-		return this.currentDisplayedOption * 50;
-	}
+    getOptionTopMargin(): number {
+        return this.currentDisplayedOption * 50;
+    }
 
-	getToolCategory(): number {
-		return this.toolService.getToolCategoryIndex();
-	}
+    getToolCategory(): number {
+        return this.toolService.getToolCategoryIndex();
+    }
+
+    chooseWorkingTool() {
+        this.displayOption(OptionType.TOOL);
+
+        this.toolService.setToolCategoryIndex(0);
+    }
 
 
 
-	chooseColor() {
-		this.displayOption(OptionType.COLOR);
-	}
-
-	chooseWorkingTool() {
-		this.displayOption(OptionType.TOOL);
-
-		this.toolService.setToolCategoryIndex(0);
-	}
-
-
-
-	newDrawingOption() {
-		//
-	}
-	saveImage() {
-		//
-	}
+    newDrawingOption() {
+        //
+    }
+    saveImage() {
+        //
+    }
 }
