2137c5f90c84325ac8ba64a37ffa38dedd6df00b
Pan <panbeining@hotmail.com>
2019-09-27 13:38:39 -0400

    hardcode blur brush
    Signed-off-by: Bei Ning Pan <bei-ning.pan@polymtl.ca>

--- client/src/services/tool/tool-options/brush.ts
+++ client/src/services/tool/tool-options/brush.ts
@@ -1,18 +1,38 @@
-import { SVGBrush } from '../../svg/element/svg.brush';
-import { Pencil } from './pencil';
 import { Renderer2 } from '@angular/core';
+import { SVGBrush } from 'src/services/svg/element/svg.brush';
+import { SVGInterface } from 'src/services/svg/element/svg.interface';
+import { ITool } from './i-tool';
+import { SVGService } from 'src/services/svg/svg.service';
 
-export class Brush extends Pencil {
-    element: SVGBrush | null;
+export class Brush implements ITool {
     FILENAME = 'brush.png';
+    element: SVGBrush | null;
 
-    constructor(renderer: Renderer2) {
-        super(renderer);
+    width: number;
+
+    constructor(private renderer: Renderer2) {
+        this.width = 1;
     }
 
     onPressed(x: number, y: number): void {
         this.element = new SVGBrush(this.renderer);
         this.element.addPoint(x, y);
         this.element.setWidth(this.width);
+    }
+
+    onMotion(x: number, y: number): void {
+        if (this.element != null) {
+            this.element.addPoint(x, y);
+        }
+    }
+    onReleased(x: number, y: number): void {
+        throw new Error('Method not implemented.');
+    }
+
+    leftClick() {
+        throw new Error('Method not implemented.');
+    }
+    leftRelease() {
+        throw new Error('Method not implemented.');
     }
 }
