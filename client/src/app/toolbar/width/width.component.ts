138a45afa111bcd674b95a7afc7b1e5a5ac50298
ross <rmyhovych@gmail.com>
2019-09-29 17:09:20 -0400

    Using emmiters and Input/Output decorators to change the width of a tool
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/app/toolbar/width/width.component.ts
+++ client/src/app/toolbar/width/width.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-width',
-  templateUrl: './width.component.html',
-  styleUrls: ['./width.component.scss']
+    selector: 'app-width',
+    templateUrl: './width.component.html',
+    styleUrls: ['./width.component.scss',],
 })
 export class WidthComponent implements OnInit {
 
-  constructor() { }
+    @Output()
+    widthEmmiter: EventEmitter<number> = new EventEmitter<number>();
+    private mWidth: number;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.mWidth = 1.0;
+    }
+
+    ngOnInit() {
+        //
+    }
+
+    get width(): number {
+        return this.mWidth;
+    }
+
+    @Input()
+    set width(width: number) {
+        this.mWidth = width;
+        this.widthEmmiter.emit(width);
+    }
 
 }
