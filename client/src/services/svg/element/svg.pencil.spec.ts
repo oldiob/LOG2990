4ae5389bc8557e2c523d486edcdc3b4183141c2e
Dalyna Pak <pocaezz@gmail.com>
2019-10-01 15:35:55 -0400

    Fix lint on Pencil
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- client/src/services/svg/element/svg.pencil.spec.ts
+++ client/src/services/svg/element/svg.pencil.spec.ts
@@ -1,6 +1,6 @@
 import { SVGPencil } from './svg.pencil';
 
-describe("SVGPencil", () => {
+describe('SVGPencil', () => {
 
     let renderer: any;
     let pencil: SVGPencil;
@@ -18,13 +18,13 @@ describe("SVGPencil", () => {
         pencil = new SVGPencil(renderer);
     });
 
-    it("should exits", () => {
+    it('should exits', () => {
         expect(pencil).toBeTruthy();
         expect(renderer.createElement).toHaveBeenCalled();
         expect(renderer.setAttribute).toHaveBeenCalled();
     });
 
-    it("should return true only if (x,y) is at where the pencil drew", () => {
+    it('should return true only if (x,y) is at where the pencil drew', () => {
 
         pencil.points.push([X, Y]);
         expect(pencil.isAt(X, Y)).toBeTruthy();
@@ -35,32 +35,32 @@ describe("SVGPencil", () => {
 
     });
 
-    it("should return false if (x,y) is nowhere the pencil drew", () => {
+    it('should return false if (x,y) is nowhere the pencil drew', () => {
         expect(pencil.isAt(X, Y)).toBeFalsy();
     });
 
-    it("should return true if the circle (x, y, r) touch one of the point drew by the pencil", () => {
+    it('should return true if the circle (x, y, r) touch one of the point drew by the pencil', () => {
         expect(pencil.isIn(X, Y, R)).toBeTruthy();
     });
 
-    it("should set the primary color", () => {
+    it('should set the primary color', () => {
         pencil.setPrimary(C);
         expect(renderer.setAttribute).toHaveBeenCalled();
     });
 
-    it("should not set the secondary color", () => {
+    it('should not set the secondary color', () => {
         pencil.setSecondary(C);
         expect(renderer.setAttribute).not.toHaveBeenCalled();
     });
 
-    it("should set width", () => {
+    it('should set width', () => {
         const width = Math.random() * 1000;
         pencil.setWidth(width);
         expect(pencil.lineWidth).toEqual(width);
         expect(renderer.setAttribute).toHaveBeenCalled();
     });
 
-    it("should add point to the points list", () => {
+    it('should add point to the points list', () => {
         pencil.addPoint(X, Y);
         expect(pencil.points).toContain([X, Y]);
         expect(renderer.setAttribute).toHaveBeenCalled();
