256929aed06c5b365e230aaa002bc1eab2c50d8b
ross <rmyhovych@gmail.com>
2019-10-01 10:47:54 -0400

    Completing the test
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/tool/tool-options/brush.spec.ts
+++ client/src/services/tool/tool-options/brush.spec.ts
@@ -2,29 +2,32 @@ import { BrushTool } from './brush';
 
 describe('BrushTool', () => {
     const create = jasmine.createSpy('create');
-    const texture = jasmine.createSpyObj('ITexture', [create]);
-    const element = jasmine.createSpyObj('SVGBrush', ['addPoint', 'setWidth', texture]);
+    const texture = jasmine.createSpyObj('ITexture', ['create', 'addPoint']);
+    const element = jasmine.createSpyObj('SVGBrush', ['addPoint', 'setWidth', 'texture']);
     const renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'setAttribute']);
-    const rendererProvider = jasmine.createSpyObj('RendererProviderService', renderer);
+    const rendererProvider = jasmine.createSpyObj('RendererProviderService', ['renderer']);
     const paletteService = jasmine.createSpyObj('PaletteService', ['getPrimary', 'getSecondary']);
+    const event = jasmine.createSpyObj('MouseEvent', ['svgX', 'svgY']);
 
     let brush: BrushTool;
-    let event: MouseEvent;
 
     beforeEach(() => {
         texture.create = create;
         texture.create.and.returnValue(null);
-        element.texture = texture;
+
         rendererProvider.renderer = renderer;
+
         brush = new BrushTool(rendererProvider, paletteService);
-        brush.element.texture = texture;
-        event = new MouseEvent('mousedown');
+        brush.element = element;
+        brush.texture = texture;
+
         event.svgX = Math.floor(Math.random() * 1000);
         event.svgY = Math.floor(Math.random() * 1000);
     });
 
     it('should exists', () => {
         expect(brush).toBeTruthy();
+        expect(brush.renderer).toEqual(renderer);
     });
 
     it('should return a SVGBrush thats not null', () => {
