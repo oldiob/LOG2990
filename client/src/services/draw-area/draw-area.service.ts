c3a59a65bdbf683c6f1ce55803afa0bec228f76b
Pan <panbeining@hotmail.com>
2019-10-04 16:52:27 -0400

    server file

--- client/src/services/draw-area/draw-area.service.ts
+++ client/src/services/draw-area/draw-area.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Message } from '../../../../common/communication/message';
+import { IndexService } from '../index/index.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DrawAreaService {
-
+  message = new BehaviorSubject<string>('');
   isSavedDrawing: boolean;
-  constructor() {
+  constructor(private basicService: IndexService) {
     this.isSavedDrawing = true;
   }
 
   save() {
     this.isSavedDrawing = true;
+    const newMessage: Message = {
+      body : 'ba',
+      title : 'ab',
+    };
+
+    alert('abc');
   }
 
   dirty() {
