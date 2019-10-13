539960a6dd687dd4f62a1059dc1cabd5149be1eb
ross <rmyhovych@gmail.com>
2019-10-13 13:03:52 -0400

    Implementing shared methods between all the shapes in the AbsSvgShape class. Adjusting SVGEllipse so it could work with it's parent abstract class
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/svg/element/svg.ellipse.ts
+++ client/src/services/svg/element/svg.ellipse.ts
@@ -1,74 +1,41 @@
 import { Renderer2 } from '@angular/core';
 import { TraceType } from 'src/services/tool/tool-options/i-shape-tool';
-import { vMinus, vMultiply, vPlus } from 'src/utils/math';
-import { SVGRect } from 'src/services/svg/element/svg.rect'
+import { vMinus, vPlus } from 'src/utils/math';
 import { AbsSvgShape } from './svg.abs-shape';
 
 export class SVGEllipse extends AbsSvgShape {
-    element: any;
-    ellipseElement: any;
 
-    surroundingRectangle: SVGRect;
-
-    startingPoint: number[];
-    center: number[];
-    radii: number[];
-
-    pointSize = 1;
     stroke = 1;
     fill = 1;
 
     traceType: TraceType;
 
-    constructor(x: number, y: number, private renderer: Renderer2) {
-        super();
-        this.element = this.renderer.createElement('g', 'svg');
-        this.ellipseElement = this.renderer.createElement('ellipse', 'svg');
-        this.renderer.appendChild(this.element, this.ellipseElement);
+    constructor(x: number, y: number, traceType: TraceType, renderer: Renderer2) {
+        super(x, y, traceType, renderer);
+
+        this.shapeElement = this.renderer.createElement('ellipse', 'svg');
+        this.renderer.appendChild(this.element, this.shapeElement);
 
         this.startingPoint = this.center = [x, y];
-        this.radii = [0, 0];
-
-        this.surroundingRectangle = new SVGRect(x, y, renderer);
-        this.surroundingRectangle.setTraceType(TraceType.BorderOnly);
-        this.surroundingRectangle.setPointSize(0.5);
-        this.surroundingRectangle.setSecondary('gray');
-
-        this.renderer.appendChild(this.element, this.surroundingRectangle.element);
-
-        this.renderer.setAttribute(this.ellipseElement, 'fill', 'none');
 
         this.setPositionAttributes();
     }
 
-    isAt(x: number, y: number): boolean {
-        switch (this.traceType) {
-            case TraceType.BorderOnly:
-                return this.isAtBorder(x, y);
-            case TraceType.FillOnly:
-                return this.isInside(x, y);
-            case TraceType.FillAndBorder:
-                return this.isInside(x, y) || this.isAtBorder(x, y);
-        }
-
-        return false;
-    }
-
-    private isAtBorder(x: number, y: number) {
-        const originalRadii = this.radii;
+    protected isAtBorder(x: number, y: number) {
+        const originalSize = this.size;
         const widthDelta = [this.pointSize / 2.0, this.pointSize / 2.0];
 
-        this.radii = vPlus(originalRadii, widthDelta);
+        this.size = vPlus(originalSize, widthDelta);
         const insideBiggerElipse = this.isInside(x, y);
 
-        this.radii = vMinus(originalRadii, widthDelta);
+        this.size = vMinus(originalSize, widthDelta);
         const insideSmallerElipse = this.isInside(x, y);
 
-        this.radii = originalRadii;
+        this.size = originalSize;
         return insideBiggerElipse && !insideSmallerElipse;
     }
 
-    private isInside(x: number, y: number) {
+    protected isInside(x: number, y: number) {
         const relativeX = x - this.center[0];
         const relativeY = y - this.center[1];
 
@@ -79,70 +46,27 @@ export class SVGEllipse extends AbsSvgShape {
     isIn(x: number, y: number, r: number): boolean {
         return true;
     }
-    setPrimary(color: string): void {
-        if (this.fill === 1) {
-            this.renderer.setAttribute(this.ellipseElement, 'fill', color);
-        }
-    }
-    setSecondary(color: string): void {
-        if (this.stroke === 1) {
-            this.renderer.setAttribute(this.ellipseElement, 'stroke', color);
-        }
-    }
-    setPointSize(pointSize: number): void {
-        this.pointSize = pointSize;
-        this.renderer.setAttribute(this.ellipseElement, 'stroke-width', this.pointSize.toString());
-    }
-    setTraceType(traceType: TraceType): void {
-        switch (traceType) {
-            case TraceType.BorderOnly:
-                this.fill = 0;
-                this.stroke = 1;
-                break;
-            case TraceType.FillOnly:
-                this.fill = 1;
-                this.stroke = 0;
-                break;
-            case TraceType.FillAndBorder:
-                this.fill = 1;
-                this.stroke = 1;
-                break;
-        }
 
-        this.renderer.setAttribute(this.ellipseElement, 'fill-opacity', `${this.fill}`);
-        this.renderer.setAttribute(this.ellipseElement, 'stroke-opacity', `${this.stroke}`);
-
-        this.traceType = traceType;
-    }
-    setCursor(x: number, y: number, shift: boolean) {
-        this.surroundingRectangle.setCursor(x, y, false);
-
-        const point: number[] = [x, y];
-        const vectorToCenter = vMultiply(vMinus(point, this.startingPoint), 0.5);
-
-        if (shift) {
-            const min = Math.min(Math.abs(vectorToCenter[0]), Math.abs(vectorToCenter[1]));
-            vectorToCenter[0] = Math.sign(vectorToCenter[0]) * min;
-            vectorToCenter[1] = Math.sign(vectorToCenter[1]) * min;
-        }
-        this.radii = [Math.abs(vectorToCenter[0]), Math.abs(vectorToCenter[1])];
-        this.center = vPlus(vectorToCenter, this.startingPoint);
+    setCursor(x: number, y: number, isShift: boolean) {
+        this.updateCoordinates(x, y, isShift);
+        this.updatePerimeter();
 
         this.setPositionAttributes();
     }
+
     release() {
-        this.renderer.removeChild(this.element, this.surroundingRectangle.element);
+        this.hidePerimeter();
     }
 
-    private setPositionAttributes() {
-        this.renderer.setAttribute(this.ellipseElement, 'cx', `${this.center[0]}`);
-        this.renderer.setAttribute(this.ellipseElement, 'cy', `${this.center[1]}`);
-        this.renderer.setAttribute(this.ellipseElement, 'rx', `${this.radii[0]}`);
-        this.renderer.setAttribute(this.ellipseElement, 'ry', `${this.radii[1]}`);
+    protected setPositionAttributes() {
+        this.renderer.setAttribute(this.shapeElement, 'cx', `${this.center[0]}`);
+        this.renderer.setAttribute(this.shapeElement, 'cy', `${this.center[1]}`);
+        this.renderer.setAttribute(this.shapeElement, 'rx', `${this.size[0]}`);
+        this.renderer.setAttribute(this.shapeElement, 'ry', `${this.size[1]}`);
     }
 
     private ellipseYs(x: number): number[] {
-        const sqrtInside = (1.0 - ((x * x) / (this.radii[0] * this.radii[0]))) * (this.radii[1] * this.radii[1]);
+        const sqrtInside = (1.0 - ((x * x) / (this.size[0] * this.size[0]))) * (this.size[1] * this.size[1]);
 
         // x outide elipse, clearly impossible to be inside
         if (sqrtInside < 0) {
