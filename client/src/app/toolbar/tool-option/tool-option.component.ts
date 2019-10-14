c3e16df1c92c660fb8fa8131227171f5509ba0bd
Dalyna Pak <pocaezz@gmail.com>
2019-10-14 15:23:05 -0400

    Try To Connect AngleEmmiter With MouseWheel
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- client/src/app/toolbar/tool-option/tool-option.component.ts
+++ client/src/app/toolbar/tool-option/tool-option.component.ts
@@ -112,7 +112,19 @@ export class ToolOptionComponent implements OnInit, IOption<ITool> {
             this.showcase.display(this.currentTool);
         }
     }
+    mouseWheelUp(event: MouseEvent): void {
+        if (this.currentTool.angle !== null) {
+        this.currentTool.angle += 15;
+        console.log('UP', this.currentTool.angle);
+        }
+    }
 
+    mouseWheelDown(event: MouseEvent): void {
+        if (this.currentTool.angle !== null) {
+        this.currentTool.angle -= 15;
+        console.log('DOWN', this.currentTool.angle);
+        }
+    }
     getFilesource(tool: ITool): string {
         return this.FILE_LOCATION + this.images.get(tool) as string;
     }
