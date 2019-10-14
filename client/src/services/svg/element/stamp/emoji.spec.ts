dd8c5ca4ae4ecd9d4ca5c6cdeba5b4c7ae523b66
Dalyna Pak <pocaezz@gmail.com>
2019-10-14 13:48:33 -0400

    Add Emoji Stamp Unit Test In Emoji.spec.ts
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

---
+++ client/src/services/svg/element/stamp/emoji.spec.ts
@@ -0,0 +1,35 @@
+import { SVGStamp } from '../svg.stamp';
+import { EmojiStamp } from './emoji';
+
+describe('EmojiStamp', () => {
+
+    let emoji: EmojiStamp;
+    let stamp: SVGStamp;
+    let renderer: any;
+    let element: any;
+
+    beforeEach(() => {
+        stamp = jasmine.createSpyObj('SVGStamp', ['renderer', 'lineWidth', 'pointsAttribute']);
+        renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'setAttribute', 'appendChild']);
+        element = jasmine.createSpyObj('any', ['']);
+
+        stamp.renderer = renderer;
+        emoji = new EmojiStamp();
+    });
+
+    it('should exists', () => {
+        expect(emoji).toBeTruthy();
+    });
+
+    it('should create emoji element', () => {
+        renderer.createElement = jasmine.createSpy().and.returnValue(element);
+        emoji.create(stamp);
+        expect(stamp.element).toEqual(element);
+        expect(renderer.createElement).toHaveBeenCalled();
+    });
+
+    it('should add point to a stamp element', () => {
+        emoji.addPoint(stamp, 0, 0);
+        expect(renderer.setAttribute).toHaveBeenCalledTimes(6);
+    });
+});
