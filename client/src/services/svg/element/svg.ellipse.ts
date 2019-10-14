4b0ad7ef8cc9633e0a73eace61b7beb5e2a1e228
ross <rmyhovych@gmail.com>
2019-10-13 23:18:10 -0400

    Renaming some constant variables to uppercase + removing abbreviations from math utils functions
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/svg/element/svg.ellipse.ts
+++ client/src/services/svg/element/svg.ellipse.ts
@@ -1,6 +1,6 @@
 import { Renderer2 } from '@angular/core';
 import { TraceType } from 'src/services/tool/tool-options/i-shape-tool';
-import { vMinus, vPlus } from 'src/utils/math';
+import { vectorMinus, vectorPlus } from 'src/utils/math';
 import { AbsSVGShape } from './svg.abs-shape';
 
 export class SVGEllipse extends AbsSVGShape {
@@ -21,25 +21,25 @@ export class SVGEllipse extends AbsSVGShape {
     }
 
     protected isAtBorder(x: number, y: number) {
-        const originalSize = this.size;
+        const ORIGINAL_SIZE = this.size;
         const widthDelta = [this.pointSize / 2.0, this.pointSize / 2.0];
 
-        this.size = vPlus(originalSize, widthDelta);
-        const insideBiggerElipse = this.isInside(x, y);
+        this.size = vectorPlus(ORIGINAL_SIZE, widthDelta);
+        const INSIDE_OUTER_ELLIPSE = this.isInside(x, y);
 
-        this.size = vMinus(originalSize, widthDelta);
-        const insideSmallerElipse = this.isInside(x, y);
+        this.size = vectorMinus(ORIGINAL_SIZE, widthDelta);
+        const INSIDE_INNER_ELLIPSE = this.isInside(x, y);
 
-        this.size = originalSize;
-        return insideBiggerElipse && !insideSmallerElipse;
+        this.size = ORIGINAL_SIZE;
+        return INSIDE_OUTER_ELLIPSE && !INSIDE_INNER_ELLIPSE;
     }
 
     protected isInside(x: number, y: number) {
-        const relativeX = x - this.center[0];
-        const relativeY = y - this.center[1];
+        const RELATIVE_X = x - this.center[0];
+        const RELATIVE_Y = y - this.center[1];
 
-        const ys: number[] = this.ellipseYs(relativeX);
-        return relativeY >= ys[1] && relativeY <= ys[0];
+        const minMaxY: number[] = this.ellipseYs(RELATIVE_X);
+        return RELATIVE_Y >= minMaxY[1] && RELATIVE_Y <= minMaxY[0];
     }
 
     isIn(x: number, y: number, r: number): boolean {
@@ -69,15 +69,15 @@ export class SVGEllipse extends AbsSVGShape {
     }
 
     private ellipseYs(x: number): number[] {
-        const sqrtInside = (1.0 - ((x * x) / (this.size[0] * this.size[0]))) * (this.size[1] * this.size[1]);
+        const INSIDE_SQRT = (1.0 - ((x * x) / (this.size[0] * this.size[0]))) * (this.size[1] * this.size[1]);
 
         // x outide elipse, clearly impossible to be inside
-        if (sqrtInside < 0) {
-            const impossibleOutcome = [-1, 1];
-            return impossibleOutcome;
+        if (INSIDE_SQRT < 0) {
+            const IMPOSSIBLE_OUTCOME = [-1, 1];
+            return IMPOSSIBLE_OUTCOME;
         }
 
-        const verticalModule = Math.sqrt(sqrtInside);
+        const verticalModule = Math.sqrt(INSIDE_SQRT);
         return [verticalModule, -verticalModule];
     }
 }
