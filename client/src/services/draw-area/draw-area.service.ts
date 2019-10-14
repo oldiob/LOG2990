b3055aad464a8a42d10935f18a80602d4b9f21d9
Pan <panbeining@hotmail.com>
2019-10-14 18:29:38 -0400

    server and client now communicate properly
    Signed-off-by: Bei Ning Pan <bei-ning.pan@polymtl.ca>

--- client/src/services/draw-area/draw-area.service.ts
+++ client/src/services/draw-area/draw-area.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Message } from '../../../../common/communication/message';
+import { DialogService } from '../dialog/dialog.service';
 import { IndexService } from '../index/index.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DrawAreaService {
+  localMessage = '1';
   message = new BehaviorSubject<string>('');
   isSavedDrawing: boolean;
   constructor(private basicService: IndexService) {
     this.isSavedDrawing = true;
+
+    this.basicService.basicGet()
+      .pipe(
+        map((message: Message) => `${message.title} ${message.body}`),
+      )
+      .subscribe(this.message);
+
+    this.basicService.getSVGObjects().subscribe((messageR : Message) => {
+      this.localMessage = messageR.body;
+    });
   }
 
   save() {
@@ -20,7 +33,7 @@ export class DrawAreaService {
       title : 'ab',
     };
 
-    alert('abc');
+    alert(this.localMessage);
   }
 
   dirty() {
