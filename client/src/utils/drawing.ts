589fea7d992755a72c3395d5d367ed5bb370461a
Olivier Dion <olivier.dion@polymtl.ca>
2019-10-15 18:17:38 -0400

    Serialization of abstract shape
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/utils/drawing.ts
+++ client/src/utils/drawing.ts
@@ -1,13 +1,19 @@
 import { SVGInterface } from 'src/services/svg/element/svg.interface';
 
 export class Drawing {
-  etiquette: string[];
-  SVGObjects: SVGInterface[];
-  name: string;
+    etiquette: string[];
+    blob: any;
+    name: string;
 
-  constructor(etiquette: string[], SVGObjects: SVGInterface[], name: string) {
-      this.etiquette = etiquette;
-      this.SVGObjects = SVGObjects;
-      this.name = name;
-  }
+    constructor(etiquette: string[], objects: SVGInterface[], name: string) {
+
+        this.blob = objects.map((obj) => {
+            if (obj.serialize) {
+                return obj.serialize();
+            }
+            return null;
+        });
+        this.etiquette = etiquette;
+        this.name = name;
+    }
 }
