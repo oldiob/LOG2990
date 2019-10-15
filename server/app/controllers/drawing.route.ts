124ae0506634209e0b88e8a16605bf853b884b18
Pan <panbeining@hotmail.com>
2019-10-15 14:14:54 -0400

    exploring sending HTML element to server

--- server/app/controllers/drawing.route.ts
+++ server/app/controllers/drawing.route.ts
@@ -9,6 +9,7 @@ import Types from '../types';
 export class DrawingRoute {
 
     router: Router;
+    page: any;
 
     constructor(
         @inject(Types.DateService) private dateService: DateService) {
@@ -32,12 +33,42 @@ export class DrawingRoute {
                 });
             });
         this.router.get('/a', (req, res) => {
-                let drawResponse = new Message();
+                // GET EXAMPLE DO NOT REMOVE SERVERSIDE
+                const drawResponse = new Message();
                 drawResponse.title = 'hello';
                 drawResponse.body = 'world';
                 res.json(drawResponse);
-                //res.send({hello: 'world'});
             });
+            /*
+        this.router.post('/add', (req, res) => {
+                const drawing = new DrawingRoute(req.body.etiquette, req.body.SVGObjects, req.body.name);
+                drawing.save()
+                    .then(drawing => {
+                    res.status(200).json({'Product': 'Product has been added successfully'});
+                    })
+                    .catch(err => {
+                    res.status(400).send('unable to save to database');
+                    });
+        });
+        */
+        this.router.post('/add', (req, res) => {
+           console.log(req.body);
+           res.status(200).json({'RESPONSE': 'Message received'});
+       });
+        this.router.post('/addM', (req, res) => {
+            // POST EXAMPLE DO NOT REMOVE SERVERSIDE
+            console.log(req.body);
+            res.status(200).json({'RESPONSE': 'Message received'});
+        });
+        this.router.post('/addTEST', (req, res) => {
+            console.log(req.body);
+            this.page = req.body;
+            res.status(200).json({'RESPONSE': 'Message received'});
+        });
+        this.router.get('/getTEST', (req, res) => {
+            console.log(this.page);
+            console.log('Sending page');
+            res.json(this.page);
+        });
     }
 }
-
