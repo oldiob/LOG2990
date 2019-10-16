efa4ad4867cc0a46541c296d4e695ecfdbf324b9
Pan <panbeining@hotmail.com>
2019-10-16 18:09:57 -0400

    refactored code
    Signed-off-by: Bei Ning Pan <bei-ning.pan@polymtl.ca>

--- client/src/services/svg/svg.service.ts
+++ client/src/services/svg/svg.service.ts
@@ -78,33 +78,33 @@ export class SVGService {
     }
     private createOpacityFilter() {
         const renderer = this.renderer;
-        const filterBlur = renderer.createElement('filter', 'svg');
-        renderer.setAttribute(filterBlur, 'id', 'opacity');
-        renderer.setAttribute(filterBlur, 'x', '-20');
-        renderer.setAttribute(filterBlur, 'width', '200');
-        renderer.setAttribute(filterBlur, 'y', '-20');
-        renderer.setAttribute(filterBlur, 'height', '200');
-        const filterBlurContent = renderer.createElement('feComponentTransfer', 'svg');
+        const filterOpacity = renderer.createElement('filter', 'svg');
+        renderer.setAttribute(filterOpacity, 'id', 'opacity');
+        renderer.setAttribute(filterOpacity, 'x', '-20');
+        renderer.setAttribute(filterOpacity, 'width', '200');
+        renderer.setAttribute(filterOpacity, 'y', '-20');
+        renderer.setAttribute(filterOpacity, 'height', '200');
+        const filterContent = renderer.createElement('feComponentTransfer', 'svg');
 
         const filterSubContent = renderer.createElement('feFuncA', 'svg');
         renderer.setAttribute(filterSubContent, 'type', 'table');
         renderer.setAttribute(filterSubContent, 'tableValues', '0 0.5');
-        renderer.appendChild(filterBlurContent, filterSubContent);
+        renderer.appendChild(filterContent, filterSubContent);
 
-        renderer.appendChild(filterBlur, filterBlurContent);
+        renderer.appendChild(filterOpacity, filterContent);
 
-        return filterBlur;
+        return filterOpacity;
   }
     private createTurbulenceFilter() {
         const renderer = this.renderer;
-        const filterBlur = renderer.createElement('filter', 'svg');
-        renderer.setAttribute(filterBlur, 'id', 'turbulence');
+        const filterTurbulence = renderer.createElement('filter', 'svg');
+        renderer.setAttribute(filterTurbulence, 'id', 'turbulence');
 
-        const filterBlurContent = renderer.createElement('feTurbulence', 'svg');
-        renderer.setAttribute(filterBlurContent, 'type', 'turbulence');
-        renderer.setAttribute(filterBlurContent, 'baseFrequency', '0.05');
-        renderer.setAttribute(filterBlurContent, 'numOctaves', '2');
-        renderer.setAttribute(filterBlurContent, 'result', 'turbulence');
+        const filterContent = renderer.createElement('feTurbulence', 'svg');
+        renderer.setAttribute(filterContent, 'type', 'turbulence');
+        renderer.setAttribute(filterContent, 'baseFrequency', '0.05');
+        renderer.setAttribute(filterContent, 'numOctaves', '2');
+        renderer.setAttribute(filterContent, 'result', 'turbulence');
 
         const filterSubContent = renderer.createElement('feDisplacementMap', 'svg');
         renderer.setAttribute(filterSubContent, 'in2', 'turbulence');
@@ -113,9 +113,9 @@ export class SVGService {
         renderer.setAttribute(filterSubContent, 'xChannelSelector', 'R');
         renderer.setAttribute(filterSubContent, 'yChannelSelector', 'G');
 
-        renderer.appendChild(filterBlur, filterBlurContent);
-        renderer.appendChild(filterBlur, filterSubContent);
+        renderer.appendChild(filterTurbulence, filterContent);
+        renderer.appendChild(filterTurbulence, filterSubContent);
 
-        return filterBlur;
+        return filterTurbulence;
     }
 }
