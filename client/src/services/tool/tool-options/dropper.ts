3f5aaaaf017700783a8a2cae6795a3814bf983aa
ross <rmyhovych@gmail.com>
2019-10-16 23:10:04 -0400

    Making the dumbass canvas read my invented svg
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/tool/tool-options/dropper.ts
+++ client/src/services/tool/tool-options/dropper.ts
@@ -1,7 +1,5 @@
 import { ITool } from './i-tool';
 import { Injectable, Renderer2 } from '@angular/core';
-import { SVGService } from 'src/services/svg/svg.service';
-import { PaletteService } from 'src/services/palette/palette.service';
 import { RendererProviderService } from 'src/services/renderer-provider/renderer-provider.service';
 
 @Injectable({
@@ -20,20 +18,34 @@ export class DropperTool implements ITool {
 
     constructor(
             rendererProvider: RendererProviderService,
-            private svgService: SVGService,
-            private paletteService: PaletteService) {
+            //private svgService: SVGService,
+            /*private paletteService: PaletteService*/) {
         this.renderer = rendererProvider.renderer;
     }
 
     onPressed(event: MouseEvent): null {
-        const canvas = this.renderer.createElement('canvas');
-        this.canvasContext = canvas.getContext('2d');
+        const canvas = document.createElement('canvas');
 
-        const svg = this.svgService.entry.nativeElement;
+        const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
 
-        const svgInner: string = svg.innerHTML;
-        console.log(svg);
-        this.canvasContext.drawImage(svg, 2, 2);
+        //const svg = this.svgService.entry.nativeElement;
+        //const svgInner: string = svg.outerHTML;
+
+        var svgImage: HTMLImageElement = new Image(100, 100);
+        svgImage.src = 'data:image/svg+xml;base64,' + window.btoa('<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><rect width="100" height="100" fill="#aaaaaa"/></svg>');
+
+        function drawBitmap() {
+            console.log('drawing bitmap');
+
+            if (ctx){
+                ctx.drawImage(svgImage, 0, 0);
+                console.log(ctx.getImageData(0, 0, 10, 10).data);
+            }
+        }
+
+        console.log('waiting to draw');
+        svgImage.onload = drawBitmap;
+
         return null;
     }
 
