3214945264a7852f41b1ce1607e0b8676debab82
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2019-10-19 20:51:35 -0400

    Add test and fix isInside and IsAtBorder functions
    Signed-Off-By: Tristan Philis <tristan.philis@gmail.com>

--- client/src/services/svg/element/svg.polygon.ts
+++ client/src/services/svg/element/svg.polygon.ts
@@ -1,18 +1,14 @@
-import { isAtLine, vectorMultiply, vectorPlus } from 'src/utils/math';
-import { AbsSVGShape } from './svg.abs-shape';
 import { RendererProvider } from 'src/services/renderer-provider/renderer-provider';
+import { vectorMinus, vectorMultiply, vectorPlus } from 'src/utils/math';
+import { AbsSVGShape } from './svg.abs-shape';
 
 export class SVGPolygon extends AbsSVGShape {
-
-    private circularPoints: number[][] = [];
-    private actualPointsPosition: number[][] = [];
-
     constructor(x: number, y: number, nSides: number, traceType: number) {
         super(x, y, traceType);
 
         this.shapeElement = RendererProvider.renderer.createElement('polygon', 'svg');
         RendererProvider.renderer.appendChild(this.element, this.shapeElement);
-
+        this.nSides = nSides;
         this.setOpacities();
         this.setCursor(x, y);
 
@@ -22,31 +18,46 @@ export class SVGPolygon extends AbsSVGShape {
         }
     }
 
+    private circularPoints: number[][] = [];
+    private actualPointsPosition: number[][] = [];
+    nSides: number;
+
     protected isAtBorder(x: number, y: number) {
-        const additionnalWidth = 10.0;
-        const width = this.pointSize + additionnalWidth;
-        const points = [
-            [this.center[0] - this.size[0], this.center[1] - this.size[1]],
-            [this.center[0] + this.size[0], this.center[1] - this.size[1]],
-            [this.center[0] + this.size[0], this.center[1] + this.size[1]],
-            [this.center[0] - this.size[0], this.center[1] + this.size[1]],
-            [this.center[0] - this.size[0], this.center[1] - this.size[1]]];
+        const ORIGINAL_SIZE = this.size;
+        const widthDelta = [this.pointSize / 2.0, this.pointSize / 2.0];
 
-        for (let i = 0; i < 4; i++) {
-            if (isAtLine([x, y], points[i], points[i + 1], width)) {
-                return true;
-            }
+        this.size = vectorPlus(ORIGINAL_SIZE, widthDelta);
+        for (const circularPoint of this.circularPoints) {
+            this.actualPointsPosition.push(vectorPlus(vectorMultiply(circularPoint, this.size[1]), this.center));
         }
+        const INSIDE_OUTER_ELLIPSE = this.isInside(x, y);
 
-        return false;
+        this.size = vectorMinus(ORIGINAL_SIZE, widthDelta);
+        for (const circularPoint of this.circularPoints) {
+            this.actualPointsPosition.push(vectorPlus(vectorMultiply(circularPoint, this.size[1]), this.center));
+        }
+        const INSIDE_INNER_ELLIPSE = this.isInside(x, y);
+
+        this.size = ORIGINAL_SIZE;
+        return INSIDE_OUTER_ELLIPSE && !INSIDE_INNER_ELLIPSE;
+
     }
 
     protected isInside(x: number, y: number) {
-        return (
-            this.center[0] - this.size[0] <= x &&
-            this.center[0] + this.size[0] >= x &&
-            this.center[1] - this.size[1] <= y &&
-            this.center[1] + this.size[1] >= y);
+
+        let inside = false;
+        for (let i = 0, j = this.actualPointsPosition.length - 1; i < this.actualPointsPosition.length; j = i++) {
+            const x1 = this.actualPointsPosition[i][0];
+            const y1 = this.actualPointsPosition[i][1];
+            const x2 = this.actualPointsPosition[j][0];
+            const y2 = this.actualPointsPosition[j][1];
+
+            const intersect = ((y1 > y) !== (y2 > y))
+            && (x < (x2 - x1) * (y - y1) / (y2 - y1) + x1);
+            if (intersect) { inside = !inside; }
+        }
+
+        return inside;
     }
 
     release() {
