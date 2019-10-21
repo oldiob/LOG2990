77c271995fa113a1f58a0e0ce95c011278ec62d0
Olivier Dion <olivier.dion@polymtl.ca>
2019-10-21 13:03:05 -0400

    Add optional isInRect method for SVG interface
    This will be use in the future for better detection of in rectangle of
    individual objects.
    
    Signed-off-by: Olivier Dion <olivier.dion@polymtl.ca>

--- client/src/services/svg/element/svg.interface.ts
+++ client/src/services/svg/element/svg.interface.ts
@@ -3,6 +3,7 @@ export interface SVGInterface {
 
     isAt(x: number, y: number): boolean;
     isIn(x: number, y: number, r: number): boolean;
+    isInRect?(x: number, y: number, w: number, h: number): boolean;
 
     setPrimary(color: string): void;
     setSecondary(color: string): void;
