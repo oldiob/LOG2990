d0cf32b411527d3c05e24f266a3e2f8a80148a71
ross <rmyhovych@gmail.com>
2019-10-22 11:30:39 -0400

    fixing ellipse/polygon tests
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/svg/element/svg.ellipse.spec.ts
+++ client/src/services/svg/element/svg.ellipse.spec.ts
@@ -1,15 +1,30 @@
 import { TraceType } from 'src/services/tool/tool-options/abs-shape-tool';
 import { SVGEllipse } from './svg.ellipse';
 import { DOMRenderer } from 'src/utils/dom-renderer';
+import { Renderer2 } from '@angular/core';
 
 describe('SVGEllipse', () => {
+    jasmine.getEnv().allowRespy(true);
 
-    let renderer: any;
+    let renderer: Renderer2;
+    let element: any;
+    let perimeter: any;
+    let baseElement: any;
+    let children: any;
+
     let ellipse: SVGEllipse;
 
     beforeEach(() => {
-        renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'appendChild', 'setAttribute']);
+        renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'setAttribute', 'appendChild']);
+
+        element = jasmine.createSpyObj('any', ['children']);
+        perimeter = jasmine.createSpyObj('any', ['']);
+        baseElement = jasmine.createSpyObj('any', ['']);
+        children = [perimeter, baseElement];
+        element.children = children;
+
         DOMRenderer.renderer = renderer;
+        spyOn(renderer, 'createElement').and.returnValue(element);
 
         ellipse = new SVGEllipse(0, 0, TraceType.FillAndBorder);
     });
