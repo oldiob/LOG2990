38c8d570a18e7d2fc857ad781e352149db3136bf
ross <rmyhovych@gmail.com>
2019-10-22 12:25:18 -0400

    finishing Dropper tests
    Signed-off-by: ross <rmyhovych@gmail.com>

---
+++ client/src/services/tool/tool-options/dropper.spec.ts
@@ -0,0 +1,61 @@
+import { SVGService } from "src/services/svg/svg.service";
+import { PaletteService } from 'src/services/palette/palette.service';
+import { DropperTool } from './dropper';
+import { Color } from 'src/utils/color';
+
+fdescribe('DropperTool', () => {
+
+    let svgService: SVGService;
+    let paletteService: PaletteService;
+
+    let dropper: DropperTool;
+
+    beforeEach(() => {
+        svgService = jasmine.createSpyObj('SVGService', ['entry']);
+        paletteService = jasmine.createSpyObj('PaletteService', ['selectPrimary', 'selectSecondary']);
+
+        dropper = new DropperTool(svgService, paletteService);
+
+        dropper.loaded = true;
+    });
+
+    it('should exists', () => {
+        expect(dropper).toBeTruthy();
+    });
+
+    it('should return if not loaded', () => {
+        dropper.loaded = false;
+
+        const mouse = new MouseEvent('', undefined);
+        dropper.onPressed(mouse);
+        expect(paletteService.selectPrimary).toHaveBeenCalledTimes(0);
+        expect(paletteService.selectSecondary).toHaveBeenCalledTimes(0);
+
+        const spy = spyOn(dropper, 'getPixelData');
+        dropper.onMotion(mouse);
+        expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+    it('shoud load correct image data', () => {
+        const imageData = jasmine.createSpyObj('ImageData', ['width', 'height', 'data']);
+        imageData.width = 2;
+        imageData.height = 1;
+        imageData.data = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        dropper.imageData = imageData;
+
+        const mouse = new MouseEvent('', undefined);
+        mouse.svgX = 0;
+        mouse.svgY = 0;
+
+        dropper.onMotion(mouse);
+        expect(dropper.currentColor).toEqual(new Color(1, 2, 3, 4));
+
+        mouse.svgX = 1;
+        mouse.svgY = 0;
+
+        dropper.onMotion(mouse);
+        expect(dropper.currentColor).toEqual(new Color(5, 6, 7, 8));
+    });
+
+});
