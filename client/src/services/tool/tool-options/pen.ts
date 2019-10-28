2be2ef2333d444a5a5f993f02e3b140adf2f1ff5
Simon Berhanu <simon1515@hotmail.com>
2019-10-28 11:04:40 -0400

    Update pen.onPressed and SVGPen getters

--- client/src/services/tool/tool-options/pen.ts
+++ client/src/services/tool/tool-options/pen.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { PaletteService } from 'src/services/palette/palette.service';
 import { SVGPen } from 'src/services/svg/element/svg.pen';
 import { ITool } from './i-tool';
+import { CmdSVG } from 'src/services/cmd/cmd.svg';
 
 @Injectable({
     providedIn: 'root',
@@ -17,22 +18,23 @@ export class PenTool implements ITool {
         this.maxWidth = 10;
     }
 
-    onPressed(event: MouseEvent): SVGPen | null {
-        if (this.element) {
-            return null;
+    onPressed(event: MouseEvent): CmdSVG | null {
+        let cmd: CmdSVG | null = null;
+        if (!this.element) {
+            const x = event.svgX;
+            const y = event.svgY;
+            this.element = new SVGPen();
+            this.element.setWidth(this.maxWidth);
+            this.element.setMinWidth(this.minWidth);
+            this.element.setMaxWidth(this.minWidth);
+            this.element.addPoint(x, y);
+            this.element.addPoint(x, y);
+
+            this.element.setPrimary(this.paletteService.getPrimary());
+
+            cmd = new CmdSVG(this.element);
         }
-        const x = event.svgX;
-        const y = event.svgY;
-        this.element = new SVGPen();
-        this.element.setWidth(this.maxWidth);
-        this.element.setMinWidth(this.minWidth);
-        this.element.setMaxWidth(this.minWidth);
-        this.element.addPoint(x, y);
-        this.element.addPoint(x, y);
-
-        this.element.setPrimary(this.paletteService.getPrimary());
-
-        return this.element;
+        return cmd;
     }
     onMotion(event: MouseEvent): void {
         if (this.element) {
