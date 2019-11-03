3d8cdd25cc8e40c42dbe0f299a363f547622227b
Simon Berhanu <simon1515@hotmail.com>
2019-11-03 18:09:34 -0500

    Extract pop ups from web client to dialogService
    Signed-off-by: Simon Berhanu <simon1515@hotmail.com>

--- client/src/services/web-client/web-client.service.ts
+++ client/src/services/web-client/web-client.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { CustomAlertComponent } from 'src/app/popups/custom-alert/custom-alert.component';
-import { LoadDrawingComponent } from 'src/app/popups/load-drawing/load-drawing.component';
+import { Observable } from 'rxjs';
 import { DialogService } from 'src/services/dialog/dialog.service';
 import { Drawing } from '../draw-area/i-drawing';
 
@@ -12,119 +9,57 @@ import { Drawing } from '../draw-area/i-drawing';
 })
 export class WebClientService {
 
-    uri = 'http://localhost:4000/draw';
     private readonly CUSTOM_URL: string = 'http://localhost:3000/draw';
-    preparedDrawings: Drawing[];
-    preparedReady: boolean;
-    saving = false;
-    loading = false;
 
     constructor(
         private dialogService: DialogService,
         private http: HttpClient) { }
 
     sendDrawing(drawing: Drawing) {
-        this.saving = true;
-        const loadingDialogRef = this.dialogService.openDialog(LoadDrawingComponent);
-        loadingDialogRef.componentInstance.content = `Saving "${drawing.name}" ...`;
-        return this.http.post(`${this.CUSTOM_URL}/add`, drawing)
+        const modal = this.dialogService.alertLoading(drawing);
+        this.http.post(`${this.CUSTOM_URL}/add`, drawing)
             .subscribe(
                 (response: Response) => {
-                    loadingDialogRef.close();
-                    this.saving = false;
-                    this.alertSuccess();
+                    modal.close();
+                    const title = 'Drawing saved';
+                    const content = 'Drawing has been saved online!';
+                    this.dialogService.alertSuccess(title, content);
                 },
                 (error: HttpErrorResponse) => {
-                    loadingDialogRef.close();
-                    this.saving = false;
-                    this.alertError(error);
+                    modal.close();
+                    this.handleError(error);
                 },
             );
     }
 
-    private alertSuccess() {
-        const snackRef = this.dialogService.openSnack(CustomAlertComponent);
-        snackRef.instance.title = 'Drawing saved';
-        snackRef.instance.content = 'Drawing has been saved online!';
-        snackRef.instance.isSuccess = true;
-    }
-
-    private alertError(error: HttpErrorResponse) {
-        if (error.status === 0) {
-            const snackRef = this.dialogService.openSnack(CustomAlertComponent);
-            snackRef.instance.title = 'Connexion Error';
-            snackRef.instance.content = 'Could not connect to server.';
-            snackRef.instance.isError = true;
-        } else if (error.status === 500) {
-            const snackRef = this.dialogService.openSnack(CustomAlertComponent);
-            snackRef.instance.title = 'Invalid drawing';
-            snackRef.instance.content = 'Server denied saving the drawing.';
-            snackRef.instance.isError = true;
-        }
-    }
-
-    addTag(id: number, tag: string) {
+    addTag(id: number, tag: string): void {
         this.http.post(`${this.CUSTOM_URL}/addtag`, { id, tag })
-            .subscribe((res: Response) => {
-                console.log(res.body);
-            },
-                (error: HttpErrorResponse) => {
-                    this.loading = false;
-                    this.alertError(error);
-                });
+            .subscribe(
+                (res: Response) => console.log(res.body),
+                (error: HttpErrorResponse) => this.handleError(error),
+            );
     }
 
-    getDrawingCount(): Observable<number> {
-        this.loading = true;
-        return this.http.get<number>(`${this.CUSTOM_URL}/drawing/count`).pipe(
-            catchError(this.handleError<number>('getDrawingCount')),
-        );
-    }
     getAllDrawings(): Observable<any> {
         return this.http.get(`${this.CUSTOM_URL}/drawing/all`);
     }
-    getDrawingsByID(id: number) {
-        return this.http.get(`${this.CUSTOM_URL}/drawing/byid/${id}`);
-    }
-    getDrawingsByTags(tags: string[], min: number, max: number) {
-        const obj: any = {
-            tags,
-            min,
-            max,
-        };
-        return this.http.post(`${this.CUSTOM_URL}/drawing/bytags`, obj);
-    }
-    deleteDrawing(id: number) {
-        return this.http.delete(`${this.CUSTOM_URL}/drawing/delete/${id}`).subscribe(
-            (response: Response) => { console.log(response); },
-            (error: HttpErrorResponse) => {
-                this.alertError(error);
-            });
+
+    deleteDrawing(id: number): void {
+        this.http.delete(`${this.CUSTOM_URL}/drawing/delete/${id}`).subscribe(
+            (response: Response) => console.log(response),
+            (error: HttpErrorResponse) => this.handleError(error),
+        );
     }
 
-    getPreparedDrawing(): Drawing[] {
-        const loadingDialogRef = this.dialogService.openDialog(LoadDrawingComponent);
-        loadingDialogRef.componentInstance.data = 'Loading';
-        let drawings: Drawing[] = [];
-        this.getAllDrawings().subscribe((savedDrawing: Drawing[]) => {
-            drawings = savedDrawing;
-            console.log(drawings);
-            this.preparedDrawings = drawings;
-            this.preparedReady = true;
-            this.loading = false;
-            loadingDialogRef.componentInstance.done();
-            return drawings;
-        },
-            (error: HttpErrorResponse) => {
-                this.loading = false;
-                this.alertError(error);
-            });
-        return drawings;
-    }
-
-    private handleError<T>(request: string, result?: T): (error: Error) => Observable<T> {
-        return (error: Error): Observable<T> => {
-            return of(result as T);
-        };
+    private handleError(error: HttpErrorResponse) {
+        if (error.status === 0) {
+            const title = 'Connexion Error';
+            const content = 'Could not connect to server.';
+            this.dialogService.alertError(title, content);
+        } else if (error.status === 500) {
+            const title = 'Invalid drawing';
+            const content = 'Server denied saving the drawing.';
+            this.dialogService.alertError(title, content);
+        }
     }
 }
