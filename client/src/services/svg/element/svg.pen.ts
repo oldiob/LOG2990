345f230d75e4a0dd03f7ba3dec2c4d44d36cfc33
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2019-11-04 21:03:10 -0500

    Add to github in order to share current work with a group member, only changes to pen are present
    Signed-Off-By: Tristan Philis <tristan.philis@polymtl.ca>

--- client/src/services/svg/element/svg.pen.ts
+++ client/src/services/svg/element/svg.pen.ts
@@ -1,189 +1,79 @@
+import { SVGInterface } from 'src/services/svg/element/svg.interface';
 import { DOMRenderer } from 'src/utils/dom-renderer';
 import { isAtLine } from 'src/utils/math';
-import { SVGInterface } from './svg.interface';
 
 export class SVGPen implements SVGInterface {
 
+    anchors: number[][] = [];
+    cursor: number[];
+    width = 5;
+    element: any;
+    polyline: any;
+    line: any;
+    circle: any;
+    minWidth: number;
+    maxWidth: number;
+
+    lineStack: any[][] = [];
+
     constructor(x: number, y: number) {
         this.cursor = [x, y];
-        this.points.push(this.cursor);
+        this.anchors.push(this.cursor);
         this.polyline = DOMRenderer.createElement('polyline', 'svg');
         DOMRenderer.setAttribute(this.polyline, 'fill', 'none');
         this.line = DOMRenderer.createElement('line', 'svg');
         DOMRenderer.setAttribute(this.line, 'fill', 'none');
-        DOMRenderer.setAttribute(this.line, 'stroke-dasharray', '4');
+        DOMRenderer.setAttribute(this.line, 'stroke', '4');
         this.element = DOMRenderer.createElement('g', 'svg');
         DOMRenderer.setAttribute(this.polyline, 'stroke', '4');
         DOMRenderer.appendChild(this.element, this.polyline);
-        DOMRenderer.setAttribute(this.polyline, 'stroke-linejoin', 'round');
-        DOMRenderer.appendChild(this.element, this.polyline);
         DOMRenderer.appendChild(this.element, this.line);
 
-        this.fullRender();
+        this.addLine();
     }
 
-    element: any;
-    polyline: any;
-    line: any;
-
-    cursor: number[];
-    oldWidth: number;
-    points: number[][] = [];
-    minWidth: number;
-    maxWidth: number;
-    lineWidth: number;
-
-    private fullRender(): void {
-        this.renderAnchors();
-        //this.renderCursor();
-    }
-
-    private renderCursor(): void {
-        let lastPoint = this.points[this.points.length - 1];
+    private addLine(): void {
+        const currentPoint = this.anchors[this.anchors.length - 1];
+        let lastPoint = this.anchors[this.anchors.length - 1];
         if (!lastPoint) {
             lastPoint = this.cursor;
         }
-        const distance = Math.sqrt(Math.pow(Math.abs(this.cursor[0] - lastPoint[0]), 2) +
-            Math.pow(Math.abs(this.cursor[1] - lastPoint[1]), 2));
 
-        this.lineWidth = this.minWidth + this.maxWidth * ( 1 / distance);
-        if (this.lineWidth > this.maxWidth) {
-            this.lineWidth = this.maxWidth;
-        } else if (this.lineWidth < this.minWidth) {
-            this.lineWidth = this.minWidth;
+        const distance = Math.sqrt(Math.pow(Math.abs(currentPoint[0] - lastPoint[0]), 2) +
+            Math.pow(Math.abs(currentPoint[1] - lastPoint[1]), 2));
+
+        this.width = this.minWidth + this.maxWidth * ( 1 / (distance / 1));
+        if (this.width > this.maxWidth) {
+            this.width = this.maxWidth;
+        } else if (this.width < this.minWidth) {
+            this.width = this.minWidth;
         }
-        this.setWidth(this.lineWidth);
-        /*
+
+        let tempLine: any;
+
+        tempLine = DOMRenderer.createElement('line', 'svg');
+        DOMRenderer.setAttribute(tempLine, 'fill', 'none');
+        DOMRenderer.setAttribute(tempLine, 'stroke', '4');
+        DOMRenderer.setAttribute(tempLine, 'stroke-width', this.width.toString());
         DOMRenderer.setAttribute(this.line, 'x1', `${lastPoint[0]}`);
         DOMRenderer.setAttribute(this.line, 'y1', `${lastPoint[1]}`);
-        DOMRenderer.setAttribute(this.line, 'x2', `${this.cursor[0]}`);
-        DOMRenderer.setAttribute(this.line, 'y2', `${this.cursor[1]}`);
-        */
-    }
-
-    private renderAnchors(): void {
-        /* DOMRenderer.setAttribute(this.polyline,
+        DOMRenderer.setAttribute(this.line, 'x2', `${currentPoint[0]}`);
+        DOMRenderer.setAttribute(this.line, 'y2', `${currentPoint[1]}`);
+        DOMRenderer.appendChild(this.element, tempLine);
+        this.lineStack.push(tempLine);
+        /*for (let i = 0; i < this.lineStack.length; i++) {
+            DOMRenderer.appendChild(this.element, this.lineStack[i]);
+        }*/
+        /*this.setWidth(this.width);
+        DOMRenderer.setAttribute(this.polyline,
             'points',
-            this.points.map((point: number[]) => `${point[0]},${point[1]}`).join(' ')); */
+            this.anchors.map((point: number[]) => `${point[0]},${point[1]}`).join(' '));*/
     }
 
-    addAnchor(x: number, y: number): void {
-        let lastPoint = this.points[this.points.length - 1];
-        if (!lastPoint) {
-            lastPoint = this.cursor;
-        }
-        this.points.push([x, y]);
-        this.renderAnchors();
-        const circle: any = DOMRenderer.createElement('circle', 'svg');
-        const distance = Math.sqrt(Math.pow(Math.abs(this.cursor[0] - lastPoint[0]), 2) +
-            Math.pow(Math.abs(this.cursor[1] - lastPoint[1]), 2));
-
-        this.lineWidth = this.minWidth + this.maxWidth * ( 1 / distance);
-        if (this.lineWidth > this.maxWidth) {
-            this.lineWidth = this.maxWidth;
-        } else if (this.lineWidth < this.minWidth) {
-            this.lineWidth = this.minWidth;
-        }
-        const radius = this.lineWidth / 2;
-
-        if (distance > 1) {
-            const nbOfCirclesToAdd = distance / 1;
-            const radiusDifference = Math.abs(this.lineWidth / 2 - this.oldWidth / 2);
-            for (let i = 1; i <= nbOfCirclesToAdd; i++) {
-
-                if (this.cursor[0] > lastPoint[0]) {
-                    const tempX = lastPoint[0] + 1 * i;
-                    DOMRenderer.setAttribute(circle, 'cx', tempX.toString());
-                } else if (this.cursor[0] < lastPoint[0]) {
-                    const tempX = this.cursor[0] + 1 * i;
-                    DOMRenderer.setAttribute(circle, 'cx', tempX.toString());
-                } else {
-                    const tempX = this.cursor[0];
-                    DOMRenderer.setAttribute(circle, 'cx', tempX.toString());
-                }
-
-                if (this.cursor[1] > lastPoint[1]) {
-                    const tempY = lastPoint[1] + 1 * i;
-                    DOMRenderer.setAttribute(circle, 'cy', tempY.toString());
-                } else if (this.cursor[1] < lastPoint[1]) {
-                    const tempY = this.cursor[1] + 1 * i;
-                    DOMRenderer.setAttribute(circle, 'cy', tempY.toString());
-                } else {
-                    const tempY = this.cursor[1];
-                    DOMRenderer.setAttribute(circle, 'cy', tempY.toString());
-                }
-
-                if (this.lineWidth > this.oldWidth) {
-                    const tempRadius = (this.oldWidth / 2) + (radiusDifference / nbOfCirclesToAdd) * i;
-                    DOMRenderer.setAttribute(circle, 'r', tempRadius.toString());
-                } else if (this.lineWidth > this.oldWidth) {
-                    const tempRadius = (this.lineWidth / 2) + (radiusDifference / nbOfCirclesToAdd) * i;
-                    DOMRenderer.setAttribute(circle, 'r', tempRadius.toString());
-                } else {
-                    const tempRadius = this.lineWidth / 2;
-                    DOMRenderer.setAttribute(circle, 'r', tempRadius.toString());
-                }
-                const tempCurrentColor = this.polyline.attributes.stroke.nodeValue;
-                DOMRenderer.setAttribute(circle, 'fill', tempCurrentColor);
-                DOMRenderer.appendChild(this.element, circle);
-            }
-
-        }
-        this.setWidth(this.lineWidth);
-        const currentColor = this.polyline.attributes.stroke.nodeValue;
-
-        DOMRenderer.setAttribute(circle, 'cx', x.toString());
-        DOMRenderer.setAttribute(circle, 'cy', y.toString());
-        DOMRenderer.setAttribute(circle, 'r', radius.toString());
-        DOMRenderer.setAttribute(circle, 'fill', currentColor);
-        DOMRenderer.appendChild(this.element, circle);
-    }
-
-    setCursor(x: number, y: number): void {
-        this.cursor = [x, y];
-        this.renderCursor();
-    }
-
-    isAt(x: number, y: number): boolean {
-        const WIDTH_MARGIN = 10.0;
-        const width: number = this.lineWidth + WIDTH_MARGIN;
-        for (let i = 0; i < this.points.length - 1; i++) {
-            if (isAtLine([x, y], this.points[i], this.points[i + 1], width)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-    isIn(x: number, y: number, r: number): boolean {
-        const tempWidth = this.lineWidth;
-        this.lineWidth += r;
-        const isInside = this.isAt(x, y);
-        this.lineWidth = tempWidth;
-
-        return isInside;
-    }
-
-    getPrimary(): string {
-        return this.element.getAttribute('stroke');
-    }
-
-    getSecondary(): string {
-        return '';
-    }
-
-    setPrimary(color: string): void {
-        for (const child of this.element.children) {
-            if (child.nodeName === 'circle') {
-                DOMRenderer.setAttribute(child, 'fill', color);
-            } else {
-                DOMRenderer.setAttribute(child, 'stroke', color);
-            }
-        }
-    }
-
-    setSecondary(color: string): void {
-        // NO OP
+    setWidth(width: number) {
+        this.width = width;
+        DOMRenderer.setAttribute(this.polyline, 'stroke-width', width.toString());
+        DOMRenderer.setAttribute(this.line, 'stroke-width', width.toString());
     }
 
     setMinWidth(minWidth: number): void {
@@ -194,26 +84,58 @@ export class SVGPen implements SVGInterface {
         this.maxWidth = maxWidth;
     }
 
-    setWidth(width: number): void {
-        this.lineWidth = width;
-        DOMRenderer.setAttribute(this.polyline, 'stroke-width', width.toString());
-        DOMRenderer.setAttribute(this.line, 'stroke-width', width.toString());
-        this.oldWidth = width;
+    getPrimary(): string {
+        const child = this.element.children[0];
+        return child.getAttribute('stroke');
+
     }
 
-    finish(): void {
-        this.points.push([this.cursor[0], this.cursor[1]]);
-        this.end();
+    getSecondary(): string {
+        return '';
     }
 
-    end(): void {
-        DOMRenderer.removeChild(this.line.parentNode, this.line);
-        this.renderAnchors();
+    setPrimary(color: string) {
+        for (const child of this.element.children) {
+            if (child.nodeName === 'circle') {
+                DOMRenderer.setAttribute(child, 'fill', color);
+            } else {
+                DOMRenderer.setAttribute(child, 'stroke', color);
+            }
+        }
+    }
+
+    setSecondary(color: string) {
+        // NO OP
+    }
+
+    isAt(x: number, y: number): boolean {
+        const additionnalWidth = 10.0;
+        const width: number = this.width + additionnalWidth;
+        for (let i = 0; i < this.anchors.length - 1; i++) {
+            if (isAtLine([x, y], this.anchors[i], this.anchors[i + 1], width)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isIn(x: number, y: number, r: number): boolean {
+        const tempWidth = this.width;
+        this.width += r;
+        const isInside = this.isAt(x, y);
+        this.width = tempWidth;
+
+        return isInside;
+    }
+
+    addAnchor(x: number, y: number): void {
+        this.cursor = [x, y];
+        this.anchors.push([x, y]);
+        this.addLine();
     }
 
     popAnchor(): void {
-        this.points.pop();
-        this.fullRender();
+        this.anchors.pop();
+        this.addLine();
     }
-
 }
--- client/src/services/tool/tool-options/pen.ts
+++ client/src/services/tool/tool-options/pen.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@angular/core';
 import { CmdSVG } from 'src/services/cmd/cmd.svg';
 import { PaletteService } from 'src/services/palette/palette.service';
 import { SVGPen } from 'src/services/svg/element/svg.pen';
-import { ITool } from './i-tool';
+import { ITool} from './i-tool';
 
 @Injectable({
     providedIn: 'root',
 })
 export class PenTool implements ITool {
-    width: number;
-    element: SVGPen | null = null;
-    minWidth: number;
-    maxWidth: number;
 
-    constructor(private paletteService: PaletteService) {
-        this.minWidth = 5;
-        this.maxWidth = 10;
-    }
+    element: SVGPen | null = null;
+    minWidth = 0.5;
+    maxWidth = 25;
+    constructor(private paletteService: PaletteService) { }
 
     onPressed(event: MouseEvent): CmdSVG | null {
         if (this.element) {
@@ -24,54 +20,29 @@ export class PenTool implements ITool {
             return null;
 
         }
-        const line = new SVGPen(event.svgX, event.svgY);
-        line.setMinWidth(this.minWidth);
-        line.setMaxWidth(this.maxWidth);
-        line.setPrimary(this.paletteService.getPrimary());
-        this.element = line;
+        const pen = new SVGPen(event.svgX, event.svgY);
+        pen.setMinWidth(this.minWidth);
+        pen.setMaxWidth(this.maxWidth);
+        pen.setPrimary(this.paletteService.getPrimary());
+        this.element = pen;
         return new CmdSVG(this.element);
+    }
+
+    onReleased(event: MouseEvent): void {
+        if (!this.element) {
+            return;
+        }
+        this.element.anchors.pop();
+        this.element.anchors.pop();
+        this.element = null;
+        return;
     }
 
     onMotion(event: MouseEvent): void {
         if (this.element) {
-        this.element.setCursor(event.svgX, event.svgY);
-        const previousElement = this.element;
-
-        const mouseEvent: MouseEvent = new MouseEvent('', undefined);
-        mouseEvent.svgX = event.svgX;
-        mouseEvent.svgY = event.svgY;
-        mouseEvent.doubleClick = false;
-
-        const action = this.onPressed(mouseEvent);
-        this.onReleased(mouseEvent);
-
-        mouseEvent.svgX = event.svgX;
-        mouseEvent.svgY = event.svgY;
-        mouseEvent.doubleClick = false;
-        this.onMotion(mouseEvent);
-
-        this.onPressed(mouseEvent);
-        this.onReleased(mouseEvent);
-
-        mouseEvent.svgX = event.svgX;
-        mouseEvent.svgY = event.svgY;
-        mouseEvent.doubleClick = false;
-        this.onMotion(mouseEvent);
-
-        this.onPressed(mouseEvent);
-        this.onReleased(mouseEvent);
-
-        this.onPressed(mouseEvent);
-        mouseEvent.doubleClick = true;
-        this.onReleased(mouseEvent);
-
-        this.element = previousElement;
+            this.element.addAnchor(event.svgX, event.svgY);
+            // this.onPressed(event);
         }
-    }
-
-    onReleased(event: MouseEvent): void {
-        this.element = null;
-        return;
     }
 
 }
