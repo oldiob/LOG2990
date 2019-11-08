5feb34fb19b47715d1bc3ff06bfc804e9cd392ad
Pan <panbeining@hotmail.com>
2019-11-08 11:24:02 -0500

    Update drawing.controller.spec.ts

--- server/app/controllers/drawing.controller.spec.ts
+++ server/app/controllers/drawing.controller.spec.ts
@@ -1,51 +1,55 @@
-// import { expect } from 'chai';
-// import { Drawing } from '../../../client/src/services/draw-area/i-drawing';
-// import { DataBaseService } from '../services/database.service';
-// import { DrawingController } from './drawing.route';
+import { expect } from 'chai';
+import { Drawing } from '../../../client/src/services/draw-area/i-drawing';
+import { DataBaseService } from '../services/database.service';
+import { DrawingController } from './drawing.controller';
 
-// describe('DrawingRoutes :', () => {
-//     let drawingController: DrawingController;
-//     let database: DataBaseService;
-//     let invalidID: Drawing;
-//     let validDrawing: Drawing;
-//     let invalidName: Drawing;
-//     let invalidTags: Drawing;
-//     invalidTags = {
-//       id: 17,
-//       name: 'rebase',
-//       tags: ['123'],
-//       holder: { entry: 'entry', elements: ['vide'] },
-//       backgroundColor: '#ffffff',
-//       width: 200,
-//       height: 200,
-//     };
-//     invalidID = {
-//         id: -1,
-//         name: 'test',
-//         tags: ['allo'],
-//         holder: { entry: 'entry', elements: ['vide'] },
-//         backgroundColor: '#ffffff',
-//         width: 200,
-//         height: 200,
-//     };
-//     validDrawing = {
-//         id: 17,
-//         name: 'test',
-//         tags: ['allo'],
-//         holder: { entry: 'entry', elements: ['vide'] },
-//         backgroundColor: '#ffffff',
-//         width: 200,
-//         height: 200,
-//         };
-//     invalidName = {
-//           id: 17,
-//           name: '',
-//           tags: ['allo'],
-//           holder: { entry: 'entry', elements: ['vide'] },
-//           backgroundColor: '#ffffff',
-//           width: 200,
-//           height: 200,
-//     };
-//     database = new DataBaseService();
-//     drawingController = new DrawingController(database);
-// });
+describe('DrawingRoutes :', () => {
+    let drawingController: DrawingController;
+    let database: DataBaseService;
+    let invalidID: Drawing;
+    let validDrawing: Drawing;
+    let invalidName: Drawing;
+    let invalidTags: Drawing;
+    invalidTags = {
+      _id: '17',
+      name: 'rebase',
+      tags: ['123'],
+      holder: { entry: 'entry', elements: ['vide'] },
+      backgroundColor: '#ffffff',
+      width: 200,
+      height: 200,
+    };
+    invalidID = {
+        _id: '-1',
+        name: 'test',
+        tags: ['allo'],
+        holder: { entry: 'entry', elements: ['vide'] },
+        backgroundColor: '#ffffff',
+        width: 200,
+        height: 200,
+    };
+    validDrawing = {
+        _id: '17',
+        name: 'test',
+        tags: ['allo'],
+        holder: { entry: 'entry', elements: ['vide'] },
+        backgroundColor: '#ffffff',
+        width: 200,
+        height: 200,
+        };
+    invalidName = {
+          _id: '17',
+          name: '',
+          tags: ['allo'],
+          holder: { entry: 'entry', elements: ['vide'] },
+          backgroundColor: '#ffffff',
+          width: 200,
+          height: 200,
+    };
+    database = new DataBaseService();
+    drawingController = new DrawingController(database);
+
+    it('should create', () => {
+        expect(drawingController.).true;
+    });
+});
