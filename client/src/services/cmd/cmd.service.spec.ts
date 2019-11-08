ca53fc29f6064f0671dbebb56e54a20ba9e95988
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2019-11-08 18:01:36 -0500

    Add the test for cmd.service.ts
    Signed-Off-By: Tristan Philis <tristan.philis@polymtl.ca>

--- client/src/services/cmd/cmd.service.spec.ts
+++ client/src/services/cmd/cmd.service.spec.ts
@@ -1,56 +1,83 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule, MatDialogModule, MatDialogRef, MatDividerModule, MatSnackBarModule, MatSnackBarRef } from '@angular/material';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
-import { async } from 'rxjs/internal/scheduler/async';
-import { EntryPointComponent } from 'src/app/popups/entry-point/entry-point.component';
-import { NewDrawingComponent } from 'src/app/popups/new-drawing/new-drawing.component';
-import {CmdService} from './cmd.service';
+import { async, TestBed } from '@angular/core/testing';
+import {CmdInterface, CmdService} from './cmd.service';
 
 describe('cmdService', () => {
-    let service: CmdService;
-
-    let openDialogSpy: jasmine.Spy;
-    const dialogRefSpyObj = jasmine.createSpyObj({ afterClosed: of({}), close: null });
+    // tslint:disable-next-line: prefer-const
+    let cmd: CmdInterface;
+    const spyObj = jasmine.createSpyObj('CmdEraser', ['eraseObject', 'execute', 'undo', 'redo']);
+    const CMD_SERVICE_MOCK = {
+      provide: CmdService,
+      useFactory: () => ({
+        instant: jasmine.createSpy('instant').and.callFake((msg) => msg),
+      }),
+    };
 
     beforeEach(async(() => {
-    TestBed.overrideModule(BrowserDynamicTestingModule, {
-      set: {
-        entryComponents: [
-          EntryPointComponent,
-          NewDrawingComponent,
-        ],
-      },
-    });
     TestBed.configureTestingModule({
-      imports: [
-        MatDividerModule,
-        MatCheckboxModule,
-        BrowserAnimationsModule,
-        BrowserDynamicTestingModule,
-        MatDialogModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatSnackBarModule,
-      ],
-      declarations: [
-        NewDrawingComponent,
-        EntryPointComponent,
-      ],
       providers: [
-        { provide: MatDialogRef },
-        { provide: MatSnackBarRef },
+        CmdService,
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents();
   }));
 
     beforeEach(() => {
-        service = TestBed.get(CmdService);
+        TestBed.configureTestingModule({ providers: [CMD_SERVICE_MOCK]});
+    });
+
+    afterEach(() => {
+      sessionStorage.clear();
+    });
+
+    it('should be created', () => {
+      expect(CMD_SERVICE_MOCK).toBeTruthy();
+    });
+
+    it('isEmptyUndosObservable should return asObservable', () => {
+      expect(CmdService.isEmptyUndosObservable).toEqual(CmdService.isEmptyUndosSubject.asObservable());
+    });
+
+    it('isEmptyRedosObservable should return asObservable', () => {
+      expect(CmdService.isEmptyRedosObservable).toEqual(CmdService.isEmptyRedosSubject.asObservable());
+    });
+
+    it('execute should do something if called with CmdInterface', () => {
+      spyOn(CmdService, 'nextRedo');
+      CmdService.execute(cmd);
+      expect(CmdService.redos.length).toEqual(0);
+    });
+
+    it('undo should be called correctly when the cmd inside is undefinied', () => {
+      const tempCmd = CmdService.undos.pop();
+      expect(CmdService.undo()).toEqual(tempCmd);
+    });
+
+    it('redo should be called correctly when the cmd inside is undefinied', () => {
+      const tempCmd = CmdService.undos.pop();
+      expect(CmdService.redo()).toEqual(tempCmd);
+    });
+
+    it('undo should be called correctly when not undefined', () => {
+      spyOn(CmdService, 'nextRedo');
+      CmdService.undos.push(spyObj);
+      CmdService.undo();
+      expect(CmdService.nextRedo).toHaveBeenCalled();
+      CmdService.undos.pop();
+      CmdService.redos.pop();
+    });
+
+    it('redo should be called correctly when not undefined', () => {
+      spyOn(CmdService, 'nextUndo');
+      CmdService.redos.push(spyObj);
+      CmdService.redo();
+      expect(CmdService.nextUndo).toHaveBeenCalled();
+      CmdService.redos.pop();
+      CmdService.undos.pop();
+    });
+
+    it('reset should be called correctly', () => {
+      CmdService.reset();
+      expect(CmdService.redos.length).toEqual(0);
     });
 
 });
