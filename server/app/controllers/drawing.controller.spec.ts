2358965ffba508db14e27d5b0c08f6c7b215f5ee
Dalyna Pak <pocaezz@gmail.com>
2019-11-08 23:45:52 -0500

    Discard Useless File
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- server/app/controllers/drawing.controller.spec.ts
+++ server/app/controllers/drawing.controller.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { Drawing } from '../../../client/src/services/draw-area/i-drawing';
 import { DataBaseService } from '../services/database.service';
 import { DrawingController } from './drawing.controller';
@@ -23,7 +23,7 @@ describe('DrawingRoutes :', () => {
         name: 'test',
         tags: ['allo'],
         holder: { entry: 'entry', elements: ['vide'] },
-        backgroundColor: '#ffffff',
+        backgroundColor: '#fffffff',
         width: 200,
         height: 200,
         };
@@ -36,28 +36,38 @@ describe('DrawingRoutes :', () => {
           width: 200,
           height: 200,
     };
-//     router = Router();
     database = new DataBaseService();
     drawingController = new DrawingController(database);
 
-    describe('isDrawingValid function:', () =>  {
-        it('should return false if the drawing name is not valid', () => {
-                (drawingController as any).isDrawingValid(invalidName);
-                expect( (drawingController as any).isDrawingValid(invalidName)).equal(false);
+    describe ('DrawingController function : ', () => {
+        it('should complete this test', (done) => {
+            assert.ok(true);
+            done();
         });
 
+        describe('isDrawingValid function:', () =>  {
+
+            it('should return false if the drawing name is not valid', () => {
+                    const invalidDrawingName = (drawingController as any).isDrawingValid(invalidName);
+                    expect( invalidDrawingName).to.equal(false);
+            });
+
+            it('should return true if the drawing name is valid', () => {
+                const validName = (drawingController as any).isDrawingValid(validDrawing);
+                expect(validName).to.equal(true);
+            });
+        });
+
+        describe('isTagValid function:', () =>  {
+            it('should return false if the drawing tags is not valid', () => {
+                    (drawingController as any).isTagValid(invalidTags);
+                    expect( (drawingController as any).isTagValid(invalidTags)).equal(false);
+            });
+
+            it('should return true if the drawing tags is valid', () => {
+                    (drawingController as any).isTagValid(validDrawing);
+                    expect( (drawingController as any).isTagValid(validDrawing.tags)).equal(true);
+            });
+        });
     });
-
-    describe('isTagValid function:', () =>  {
-        it('should return false if the drawing tags is not valid', () => {
-                (drawingController as any).isTagValid(invalidTags);
-                expect( (drawingController as any).isTagValid(invalidTags)).equal(false);
-        });
-
-        it('should return true if the drawing tags is valid', () => {
-                (drawingController as any).isTagValid(validDrawing);
-                expect( (drawingController as any).isTagValid(validDrawing.tags)).equal(true);
-        });
-    });
-
 });
