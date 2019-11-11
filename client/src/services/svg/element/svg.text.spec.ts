beac2871a5e14c9fdcaa54872cb20a6524b2780b
Dalyna Pak <pocaezz@gmail.com>
2019-11-11 13:13:06 -0500

    Fix unit test text
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- client/src/services/svg/element/svg.text.spec.ts
+++ client/src/services/svg/element/svg.text.spec.ts
@@ -8,7 +8,7 @@ fdescribe('SVGText', () => {
     let currentSubElement: any;
     let keyService: KeyService;
     let renderer: any;
-    let event: MouseEvent;
+    let subElements: any;
     const textAlign = 'start';
     const fontFamily = 'Arial';
     const fontStyle = 'italic';
@@ -19,44 +19,52 @@ fdescribe('SVGText', () => {
         renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'setAttribute', 'appendChild']);
         DOMRenderer.renderer = renderer;
         currentSubElement = jasmine.createSpyObj('any', ['innerHTML']);
-        event = new MouseEvent('click');
         keyService = new KeyService();
-        svgText = new SVGText(keyService, event.svgX, event.svgY, fontFamily, fontSize, textAlign, fontStyle, fontWeight);
-        svgText.currentSubElement = currentSubElement;
+        spyOn(renderer, 'createElement').and.returnValue(currentSubElement);
+        svgText = new SVGText(keyService, 0, 0, fontFamily, fontSize, textAlign, fontStyle, fontWeight);
+        subElements = [];
+        svgText.subElements = subElements;
     });
 
     it('should create', () => {
         expect(svgText).toBeTruthy();
     });
 
-    // it('should set primary color properly', () => {
-    //   spyOn(svgText, 'setPrimary');
-    //   svgText.setPrimary('000000');
-    //   expect(renderer.setAttribute).toHaveBeenCalled();
-    // });
-    // it('should set font size properly', () => {
-    //   expect(renderer.setAttribute).toHaveBeenCalled();
-    // });
-    // it('should set font family properly', () => {
-    //   expect(renderer.setAttribute).toHaveBeenCalled();
-    // });
-    // it('should set font style properly', () => {
-    //   expect(renderer.setAttribute).toHaveBeenCalled();
-    // });
-    // it('should set font weight properly', () => {
-    //   expect(renderer.setAttribute).toHaveBeenCalled();
-    // });
+    it('should set primary color properly', () => {
+      spyOn(svgText, 'setPrimary');
+      svgText.setPrimary('000000');
+      expect(renderer.setAttribute).toHaveBeenCalled();
+    });
+
+    it('should set font size properly', () => {
+      expect(renderer.setAttribute).toHaveBeenCalled();
+    });
+
+    it('should set font family properly', () => {
+      expect(renderer.setAttribute).toHaveBeenCalled();
+    });
+
+    it('should set font style properly', () => {
+      expect(renderer.setAttribute).toHaveBeenCalled();
+    });
+
+    it('should set font weight properly', () => {
+      expect(renderer.setAttribute).toHaveBeenCalled();
+    });
+
     // it('should set text align properly', () => {
     //   expect(renderer.setAttribute).toHaveBeenCalledTimes(svgText.subElements.length);
     // });
-    // it('should create a placeholder properly', () => {
-    //     svgText.setCurrentPlaceholder();
-    //     expect(svgText.currentSubElement.innerHTML).toBe('i');
-    // });
-    // it('should create line break properly', () => {
-    //     svgText.setLineBreak();
-    //     expect(renderer.setAttribute).toHaveBeenCalledTimes(3);
-    //     expect(renderer.appendChild).toHaveBeenCalled();
-    // });
+
+    it('should create a placeholder properly', () => {
+        svgText.setCurrentPlaceholder();
+        expect(svgText.currentSubElement.innerHTML).toBe('i');
+    });
+
+    it('should create line break properly', () => {
+        svgText.setLineBreak();
+        expect(renderer.setAttribute).toHaveBeenCalledTimes(13);
+        expect(renderer.appendChild).toHaveBeenCalled();
+    });
 
 });
