464d7596acefab3ab265b7a78533e1991792409c
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2019-11-11 18:21:16 -0500

    Continue test for eraser.ts
    Signed-Off-By: Tristan philis <tristan.philis@polymtl.ca>

--- client/src/services/tool/tool-options/eraser.spec.ts
+++ client/src/services/tool/tool-options/eraser.spec.ts
@@ -3,6 +3,7 @@ import { SVGService } from 'src/services/svg/svg.service';
 import { DOMRenderer } from 'src/utils/dom-renderer';
 import { MyInjector } from 'src/utils/injector';
 import { EraserTool } from './eraser';
+import { SVGPencil } from 'src/services/svg/element/svg.pencil';
 
 fdescribe('EraserTool', () => {
     let renderer: any;
@@ -11,12 +12,15 @@ fdescribe('EraserTool', () => {
     let event: MouseEvent;
     let container: any | null;
     let children: any;
+    let spySVGAbstract;
 
     beforeEach(() => {
         MyInjector.injector = jasmine.createSpyObj('Injector', ['get']);
         renderer = jasmine.createSpyObj('Renderer2', ['createElement', 'setAttribute', 'removeChild', 'appendChild']);
-        svgService = jasmine.createSpyObj('SVGService', ['getPrimary', 'getSecondary', 'entry']);
+        svgService = jasmine.createSpyObj('SVGService', ['findIn', 'getPrimary', 'getSecondary', 'entry', 'addObject']);
         container = jasmine.createSpyObj('any | null', ['children']);
+        spySVGAbstract = jasmine.createSpyObj('SVGAbstract', ['forEach']);
+        spyOn(svgService, 'findIn').and.returnValue(spySVGAbstract);
         DOMRenderer.renderer = renderer;
         eraser = new EraserTool(svgService);
         (eraser as any).svgService = svgService;
@@ -34,17 +38,32 @@ fdescribe('EraserTool', () => {
         expect(eraser).toBeTruthy();
     });
 
-    it('OnPressed should accomplished its task normally', () => {
+    it('OnPressed should equal to a new CmdEraser', () => {
         const tempCmd = new CmdEraser();
         eraser.activated = false;
         eraser.onPressed(event);
         expect(eraser.onPressed(event)).toEqual(tempCmd);
     });
 
-    it('On release should accomplish all task correctly', () => {
+    it('On release should turn activated to false', () => {
         eraser.activated = true;
         eraser.onReleased(event);
         expect(eraser.activated).toEqual(false);
+    });
+
+    it('On motion should call deleteAll if activated is equal to true', () => {
+        eraser.activated = true;
+        eraser.onMotion(event);
+        expect(eraser.objectsOnHold.length).toBeUndefined();
+    });
+
+    it('On motion should call appendChild if activated is equal to true while objectsOnHold is not empty', () => {
+        spyOn(DOMRenderer, 'appendChild');
+        const tempPencil = new SVGPencil();
+        (eraser as any).svgService.addObject(tempPencil);
+        eraser.activated = false;
+        eraser.onMotion(event);
+        expect(DOMRenderer.appendChild).toHaveBeenCalled();
     });
 
 });
