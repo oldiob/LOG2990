8bfbdb6309f9937ec5711e0c3bf2bec42e5941a3
Dalyna Pak <pocaezz@gmail.com>
2019-11-10 19:20:29 -0500

    Add compile should create
    Signed-off-by: Dalyna Pak <pocaezz@gmail.com>

--- client/src/services/cmd/cmd.eraser.spec.ts
+++ client/src/services/cmd/cmd.eraser.spec.ts
@@ -1,15 +1,22 @@
+import { async, TestBed } from '@angular/core/testing';
+import { MyInjector } from 'src/utils/injector';
 import {CmdEraser} from './cmd.eraser';
 
 fdescribe('cmdEraser', () => {
     let cmdEraser: CmdEraser;
-    const svgSpy = jasmine.createSpyObj('SVGService', ['removeObject' ]);
-    /*const spyObj = jasmine.createSpyObj('SVGPencil', ['isAtAdjusted', 'isIn', 'getPrimary', 'getSecondary',
-                                                        'setPrimary', 'setSecondary', 'setWidth', 'addPoint',
-                                                        'pointsAttribute']);*/
+    // const svgSpy = jasmine.createSpyObj('SVGService', ['removeObject' ]);
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [CmdEraser],
+        }).compileComponents();
+    }));
 
     beforeEach(() => {
-          cmdEraser = new CmdEraser();
-          cmdEraser.svg = svgSpy;
+        MyInjector.injector = jasmine.createSpyObj('Injector', ['get']);
+        cmdEraser = new CmdEraser();
+        //   cmdEraser = TestBed.get(CmdEraser);
+        // cmdEraser.svg = MyInjector.get(svgSpy);
       });
 
     it('should be created', () => {
