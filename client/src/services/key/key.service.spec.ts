7a223b084332dc8b860c7526160085d7570d8178
Pan <panbeining@hotmail.com>
2019-11-12 11:54:51 -0500

    add all test for key service
    Signed-off-by: Bei Ning Pan <bei-ning.pan@polymtl.ca>

--- client/src/services/key/key.service.spec.ts
+++ client/src/services/key/key.service.spec.ts
@@ -3,10 +3,30 @@ import { TestBed } from '@angular/core/testing';
 import { KeyService } from './key.service';
 
 describe('KeyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: KeyService = TestBed.get(KeyService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: KeyService = TestBed.get(KeyService);
+        expect(service).toBeTruthy();
+    });
+    it('should disable keys properly', () => {
+        const service: KeyService = TestBed.get(KeyService);
+        service.disableKeys();
+        expect(service.isShortcutsEnabled).toBeFalsy();
+    });
+    it('should enable keys properly', () => {
+        const service: KeyService = TestBed.get(KeyService);
+        service.enableKeys();
+        expect(service.isShortcutsEnabled).toBeTruthy();
+    });
+    it('should enable text edit properly', () => {
+        const service: KeyService = TestBed.get(KeyService);
+        service.enableTextEdit();
+        expect(service.isTextEnabled).toBeTruthy();
+    });
+    it('should disable text edit properly', () => {
+        const service: KeyService = TestBed.get(KeyService);
+        service.disableTextEdit();
+        expect(service.isTextEnabled).toBeFalsy();
+    });
 });
