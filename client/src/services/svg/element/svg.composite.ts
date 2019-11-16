62b6e477626225bfc74e42726fc6be5290119273
ross <rmyhovych@gmail.com>
2019-11-16 15:12:53 -0500

    Finish SVGComposite
    Signed-off-by: ross <rmyhovych@gmail.com>

--- client/src/services/svg/element/svg.composite.ts
+++ client/src/services/svg/element/svg.composite.ts
@@ -1,3 +1,4 @@
+import { vectorMinus, vectorMultiply, vectorPlus } from 'src/utils/math';
 import { SVGAbstract } from './svg.abstract';
 
 export class SVGComposite extends SVGAbstract {
@@ -11,6 +12,10 @@ export class SVGComposite extends SVGAbstract {
 
     addChild(child: SVGAbstract) {
         this.children.push(child);
+    }
+
+    isAt(x: number, y: number): boolean {
+        return false;
     }
 
     isIn(x: number, y: number, r: number): boolean {
@@ -31,6 +36,41 @@ export class SVGComposite extends SVGAbstract {
     }
     protected isAtAdjusted(x: number, y: number): boolean {
         return false;
+    }
+
+    set position(newPosition: number[]) {
+        const center: number[] = this.position;
+        for (const child of this.children) {
+            const offset = vectorMinus(child.position, center);
+            child.position = vectorPlus(newPosition, offset);
+        }
+    }
+
+    get position() {
+        const rect = this.domRect;
+
+        return vectorPlus([rect.x, rect.y], vectorMultiply([rect.width, rect.height], 0.5));
+    }
+
+    translate(x: number, y: number): void {
+        for (const child of this.children) {
+            child.translate(x, y);
+        }
+    }
+
+    get domRect(): DOMRect {
+        const xRange = [Infinity, -Infinity];
+        const yRange = [Infinity, -Infinity];
+        this.children.forEach((child: SVGAbstract) => {
+            const rect: DOMRect = child.domRect;
+
+            xRange[0] = Math.min(xRange[0], rect.x);
+            yRange[0] = Math.min(yRange[0], rect.y);
+            xRange[1] = Math.max(xRange[1], rect.x + rect.width);
+            yRange[1] = Math.max(yRange[1], rect.y + rect.height);
+        });
+
+        return new DOMRect(xRange[0], yRange[0], xRange[1] - xRange[0], yRange[1] - yRange[0]);
     }
 
 }
