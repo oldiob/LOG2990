8ea6effc1c5fa1ac1881effbdb3238caffe33bca
TristanPhilis <46928560+TristanPhilis@users.noreply.github.com>
2019-12-01 13:58:50 -0500

    Start test for matrix.ts
    Signed-off-By: Tristan Philis <tristan.philis@polymtl.ca>

--- client/src/utils/matrix.spec.ts
+++ client/src/utils/matrix.spec.ts
@@ -3,17 +3,19 @@ import { MatrixSVG } from './matrix';
 describe('MatrixSVG', () => {
 
     let mat: MatrixSVG;
-    // let tx: number;
-    // let ty: number;
-    // let sx: number;
-    // let sy: number;
+    let tx: number;
+    let ty: number;
+    let a: number;
+    let sx: number;
+    let sy: number;
 
     beforeEach(() => {
         mat = new MatrixSVG();
-        // tx = Math.random();
-        // ty = Math.random();
-        // sx = Math.random();
-        // sy = Math.random();
+        tx = Math.random();
+        ty = Math.random();
+        a = Math.random();
+        sx = Math.random();
+        sy = Math.random();
     });
 
     it('should be the identity matrix', () => {
@@ -23,16 +25,33 @@ describe('MatrixSVG', () => {
 
     it('should translate', () => {
         // TODO - Implements me
-        expect(true).toBeTruthy();
+        const tempMatrix = new MatrixSVG();
+        tempMatrix.arr[2] += tx;
+        tempMatrix.arr[5] += ty;
+        expect(mat.translate(tx, ty)).toEqual(tempMatrix);
     });
 
     it('should rotate', () => {
         // TODO - Implements me
-        expect(true).toBeTruthy();
+        const tempMatrix = new MatrixSVG();
+        const cos = Math.cos(a);
+        const sin = Math.sin(a);
+        const other: MatrixSVG = new MatrixSVG();
+        other.arr[0] = cos;
+        other.arr[1] = -sin;
+        other.arr[3] = sin;
+        other.arr[4] = cos;
+        tempMatrix.mul(other);
+        expect(mat.rotate(a)).toEqual(tempMatrix);
     });
 
     it('should scale', () => {
         // TODO - Implements me
+        const tempMatrix = new MatrixSVG();
+        const other: MatrixSVG = new MatrixSVG();
+        other.arr[0] = sx;
+        other.arr[4] = sy;
+        tempMatrix.mul(other);
         expect(true).toBeTruthy();
     });
 
@@ -43,5 +62,9 @@ describe('MatrixSVG', () => {
         const inv: MatrixSVG = mat.inverse();
         inv.mul(mat);
         expect(inv.isIdentity()).toBeTruthy();
+    });
+
+    it('isIdentity should return true if Math.abs(this.arr[i] - expect) < 10E-4', () => {
+        expect(mat.isIdentity()).toBeTruthy();
     });
 });
