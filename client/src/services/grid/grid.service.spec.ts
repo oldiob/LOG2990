9cf2b4627b87328bb69804ad1f6fff956adc92be
Simon Berhanu <simon1515@hotmail.com>
2019-12-04 15:45:44 -0500

    Fix grid-service realDistanceToMove() expect
    Signed-off-by: Simon Berhanu <simon1515@hotmail.com>

--- client/src/services/grid/grid.service.spec.ts
+++ client/src/services/grid/grid.service.spec.ts
@@ -1,4 +1,7 @@
+import { Compass } from 'src/utils/compass';
+import { vectorMinus } from 'src/utils/math';
 import { DOMRenderer } from '../../utils/dom-renderer';
+import { SelectorBox } from '../tool/tool-options/selector-box';
 import { GridService } from './grid.service';
 
 describe('GridService', () => {
@@ -6,6 +9,7 @@ describe('GridService', () => {
     let renderer: any;
     let ref: any;
     let service: GridService;
+    let svg: any;
 
     const WIDTH = Math.random() * 1000;
     const HEIGHT = Math.random() * 1000;
@@ -14,6 +18,7 @@ describe('GridService', () => {
         renderer = jasmine.createSpyObj('Renderer2', ['appendChild', 'removeChild', 'setAttribute', 'createElement']);
         DOMRenderer.renderer = renderer;
         ref = jasmine.createSpyObj('ElementRef', ['nativeElement']);
+        svg = jasmine.createSpyObj('SVGService', ['addElement', 'removeElement']);
         service = new GridService();
         service.width = WIDTH;
         service.height = HEIGHT;
@@ -54,6 +59,21 @@ describe('GridService', () => {
         service.draw();
         expect(renderer.setAttribute).toHaveBeenCalled();
         expect(renderer.appendChild).toHaveBeenCalled();
+    });
+
+    it('should calculate the real distance to move', () => {
+        const pos: number[] = [27, 4];
+        const prevPos: number[] = [4, 4];
+        const box: SelectorBox = new SelectorBox(svg);
+        box.setBox(new DOMRect(0, 0, 8, 8));
+        service.anchor = Compass.E;
+        (service as any).mStep = 5;
+        const move: number[] = service.realDistanceToMove(box, pos, prevPos);
+        console.log(move);
+        console.log((service as any).mStep);
+        const result = vectorMinus(pos, prevPos);
+        expect(result[0]).toEqual(move[0]);
+        expect(result[1]).toEqual(move[1]);
     });
 
 });
